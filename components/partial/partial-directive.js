16b6c4695d6a51a4a7a95683c337704e4aee0be7
David DeCarmine <hworld@holo-world.com>
2016-02-04 19:53:54 +1100

    New gj-partial directive. Can be used in place of ng-include to load in content that you know is in the template cache. This speeds up template includes when splitting out a template into multiple files.

---
+++ components/partial/partial-directive.js
@@ -0,0 +1,22 @@
+// http://zachsnow.com/#!/blog/2014/angularjs-faster-ng-include/
+angular.module( 'gj.Partial' ).directive( 'gjPartial', function( $templateCache )
+{
+	return {
+		restrict: 'A',
+		priority: 400,  // Same as ng-include.
+		compile: function( element, attrs )
+		{
+			var templateName = attrs.gjPartial;
+			if ( !templateName ) {
+				throw new Error( 'gjPartial: expected template name' );
+			}
+
+			var template = $templateCache.get( templateName );
+			if ( angular.isUndefined( template ) ) {
+				throw new Error( 'gjPartial: unknown template ' + templateName );
+			}
+
+			element.html( template );
+		}
+	}
+} );
