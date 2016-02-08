c75a9b6419e976c7b4222997199c1e3da82b2db2
David DeCarmine <hworld@holo-world.com>
2016-02-08 22:21:51 +1100

    Fixing up forum posts and topics for latest forum stuff.

--- components/forum/post/post-model.js
+++ components/forum/post/post-model.js
@@ -1,11 +1,44 @@
-angular.module( 'gj.Forum.Post' ).factory( 'Forum_Post', function( Model )
+angular.module( 'gj.Forum.Post' ).factory( 'Forum_Post', function( Model, User )
 {
 	function Forum_Post( data )
 	{
 		if ( data ) {
 			angular.extend( this, data );
+
+			if ( data.user ) {
+				this.user = new User( data.user );
+			}
+
+			if ( data.replied_to ) {
+				this.replied_to = new User( data.replied_to );
+			}
+
+			if ( data.modified_by_user ) {
+				this.modified_by_user = new User( data.modified_by_user );
+			}
 		}
 	}
+
+	Forum_Post.STATUS_ACTIVE = 'active';
+	Forum_Post.STATUS_SPAM = 'spam';
+	Forum_Post.STATUS_REMOVED = 'removed';
+
+	Forum_Post.prototype.$save = function()
+	{
+		var url = '/web/forums/posts/save/' + this.topic_id;
+		var query = '';
+
+		if ( this.reply_to ) {
+			query = '?reply_to=' + this.reply_to;
+		}
+
+		if ( !this.id ) {
+			return this.$_save( url + query, 'forumPost' );
+		}
+		else {
+			return this.$_save( url + '/' + this.id + query, 'forumPost' );
+		}
+	};
 
 	return Model.create( Forum_Post );
 } );
