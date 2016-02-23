0aa9dba023478388a7ffecdeed65d4002e69bd03
David DeCarmine <hworld@holo-world.com>
2016-02-23 15:27:56 +1000

    Changed autoscroll service to use the new History service instead of managing it's own popstate checking.

--- components/scroll/auto-scroll/auto-scroll-directive.js
+++ components/scroll/auto-scroll/auto-scroll-directive.js
@@ -1,13 +1,10 @@
-/**
- * TODO: Some times the browser's pop event fires after state change success. Does it affect things?
- */
-angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q, $window, $document, $timeout, $parse, $position, AutoScroll, $state )
+angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q, $window, $document, $timeout, $parse, $position, AutoScroll, History, $state )
 {
 	return {
 		link: function( scope, element, attrs )
 		{
 			var startWatcher, successWatcher, stateWatcher;
-			var popEventFired = false;
+			// var popEventFired = false;
 
 			// If this element is the document body, then set the element to scroll to be the document.
 			// This is what the browser scrolls by default.
@@ -18,10 +15,10 @@ angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q
 
 			// http://stackoverflow.com/questions/16355549/window-history-length-maximum-value/16603692#16603692
 			// https://developer.mozilla.org/en-US/docs/WindowEventHandlers.onpopstate
-			function watchPop( event )
-			{
-				popEventFired = true;
-			}
+			// function watchPop( event )
+			// {
+			// 	popEventFired = true;
+			// }
 
 			function doScroll( to )
 			{
@@ -33,18 +30,21 @@ angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q
 					// If the popstate event fired it means the user was either going back or forward through history manually
 					// in the browser. In that case, we want to attempt to scroll.
 					var state = AutoScroll.getState( to );
-					if ( popEventFired && state && state.scroll > 0 ) {
+					if ( History.inHistorical && state && state.scroll > 0 ) {
 
 						// We need to let angular compile before attempting to scroll.
 						// TODO: This causes a flicker of content before scrolling. Would be great to somehow get rid of this flicker.
 						$timeout( function()
 						{
+							console.log( 'set scroll top' );
 							scrollElem.scrollTop( state.scroll );
-						}, 0 );
+						}, 0, false );
 					}
 					else {
 						var anchor = AutoScroll.anchor();
 						if ( anchor ) {
+
+							console.log( 'anchor scroll' );
 
 							// We only scroll to the anchor if they're scrolled past it currently.
 							var offset = $position.offset( anchor );
@@ -53,12 +53,16 @@ angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q
 							}
 						}
 						else {
-							scrollElem.scrollTop( 0 );
+							console.log( 'reset scroll top' );
+							$timeout( function()
+							{
+								scrollElem.scrollTop( 0 );
+							}, 0, false );
 						}
 					}
 				}
 
-				popEventFired = false;
+				// popEventFired = false;
 
 				// Clear out no-scroll that may be set now that we've gone to a new location.
 				AutoScroll.noScroll( false );
@@ -78,7 +82,7 @@ angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q
 					doScroll( $state.href( arguments[1], arguments[2] ) );
 				} );
 
-				angular.element( $window ).on( 'popstate', watchPop );
+				// angular.element( $window ).on( 'popstate', watchPop );
 			}
 
 			function deregisterWatchers()
@@ -91,7 +95,7 @@ angular.module( 'gj.Scroll.AutoScroll' ).directive( 'gjAutoScroll', function( $q
 					successWatcher();
 				}
 
-				angular.element( $window ).off( 'popstate', watchPop );
+				// angular.element( $window ).off( 'popstate', watchPop );
 			}
 
 			// We only activate when the directive attribute evaluates to true.
