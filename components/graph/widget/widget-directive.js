664fee02ee9dbab849f416a0c78001d088babd50
David DeCarmine <hworld@holo-world.com>
2016-03-28 21:11:03 -0700

    Modified graphing components to allow more types and more flexibility.

--- components/graph/widget/widget-directive.js
+++ components/graph/widget/widget-directive.js
@@ -9,19 +9,69 @@ angular.module( 'gj.Graph.Widget' ).directive( 'gjGraphWidget', function( Graph 
 			hideTable: '=graphWidgetHideTable',
 		},
 		controllerAs: 'ctrl',
+		controller: function( $attrs, Api )
+		{
+			var _this = this;
+			this.isLoading = true;
+
+			$attrs.$observe( 'graphWidgetUrl', function( url )
+			{
+				// _this.isLoading = true;
+				Api.sendRequest( url, null, { detach: true } ).then( function( response )
+				{
+					if ( response.data && angular.isArray( response.data ) ) {
+						_this.graphData = Graph.createGraphData( response.data );
+					}
+
+					_this.isLoading = false;
+				} );
+			} );
+		},
+	};
+} );
+
+/*
+angular.module( 'gj.Graph.Widget' ).directive( 'gjGraphWidget', function( Graph )
+{
+	return {
+		restrict: 'E',
+		templateUrl: '/lib/gj-lib-client/components/graph/widget/widget.html',
+		scope: {},
+		bindToController: {
+			url: '@graphWidgetUrl',
+			hideTable: '=graphWidgetHideTable',
+			_graphData: '=graphWidgetData',
+		},
+		controllerAs: 'ctrl',
 		controller: function( Api )
 		{
 			var _this = this;
 			this.isLoading = true;
 
-			Api.sendRequest( this.url, null, { detach: true } ).then( function( response )
-			{
-				if ( response.data && angular.isArray( response.data ) ) {
-					_this.graphData = Graph.createGraphData( response.data );
-				}
+			if ( angular.isDefined( this._graphData ) ) {
+				this.isLoading = false;
+			}
+			else {
+				Api.sendRequest( this.url, null, { detach: true } ).then( function( response )
+				{
+					if ( response.data && angular.isArray( response.data ) ) {
+						_this.graphData = Graph.createGraphData( response.data );
+					}
 
-				_this.isLoading = false;
+					_this.isLoading = false;
+				} );
+			}
+
+			$scope.$watch( function()
+			{
+				return _this._graphData;
+			},
+			function( graphData )
+			{
+				_this.graphData = graphData;
 			} );
 		},
 	};
 } );
+
+*/
