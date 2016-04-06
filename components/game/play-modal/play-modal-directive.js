94567be50c0a9010f12b0aef5e9d9dd0a69ddcc9
David DeCarmine <hworld@holo-world.com>
2016-04-06 13:20:57 +0100

    New game play modal that has a more fullscreen experience.
    Currently running as a sort of split test.

---
+++ components/game/play-modal/play-modal-directive.js
@@ -0,0 +1,32 @@
+angular.module( 'gj.Game.PlayModal' ).directive( 'gjGamePlayModal', function()
+{
+	return {
+		templateUrl: '/lib/gj-lib-client/components/game/play-modal/play-modal.html',
+		scope: {
+			game: '=gjGame',
+			build: '=gjBuild',
+			canMinimize: '=',
+			minimize: '&',
+			maximize: '&',
+			close: '&',
+		},
+		controllerAs: 'ctrl',
+		bindToController: true,
+		controller: function( $injector, $document, $element )
+		{
+			var _this = this;
+
+			// var body = $document.find( 'body' ).eq( 0 );
+
+			// this.canMinimize = $injector.has( 'Minbar' );
+
+			// If the game has ads turned off, set it as "shown" initially.
+			this.adShown = !this.game.ads_enabled;
+
+			this.onAdShown = function()
+			{
+				this.adShown = true;
+			};
+		}
+	};
+} );
