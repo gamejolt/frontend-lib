18d129893406e0cf23fddce5ce3ae8b206b1ef4d
David DeCarmine <hworld@holo-world.com>
2016-05-17 14:47:20 +0100

    You can now trigger popovers through hover event.

--- components/popover/popover-trigger-directive.js
+++ components/popover/popover-trigger-directive.js
@@ -4,26 +4,56 @@ angular.module( 'gj.Popover' ).directive( 'gjPopoverTrigger', function( Popover,
 		restrict: 'A',
 		link: function( scope, element, attrs )
 		{
-			element.on( 'click', function( event )
+			function getPopover()
 			{
-				scope.$apply( function()
+				return Popover.getPopover( $interpolate( attrs.gjPopoverTrigger )( scope ) );
+			}
+
+			if ( !attrs.popoverTriggerEvent || attrs.popoverTriggerEvent == 'click' ) {
+				element.on( 'click', function( event )
 				{
-					var popoverCtrl = Popover.getPopover( $interpolate( attrs.gjPopoverTrigger )( scope ) );
-					if ( popoverCtrl ) {
+					scope.$apply( function()
+					{
+						var popover = getPopover();
+						if ( popover ) {
 
-						// First make sure all popovers currently showing are hidden.
-						Popover.hideAll( { skip: popoverCtrl } );
+							// First make sure all popovers currently showing are hidden.
+							Popover.hideAll( { skip: popover } );
 
-						// Trigger the popover.
-						// Will either hide or show depending on its status.
-						popoverCtrl.trigger( element );
+							// Trigger the popover.
+							// Will either hide or show depending on its status.
+							popover.trigger( element );
 
-						// Make sure this event doesn't bubble up to our global $document click event.
-						// If we let it bubble, this popover will close.
-						event.stopPropagation();
-					}
+							// Make sure this event doesn't bubble up to our global $document click event.
+							// If we let it bubble, this popover will close.
+							event.stopPropagation();
+						}
+					} );
 				} );
-			} );
+			}
+			else if ( attrs.popoverTriggerEvent == 'hover' ) {
+				element.on( 'mouseenter', function( event )
+				{
+					scope.$apply( function()
+					{
+						var popover = getPopover();
+						if ( popover ) {
+							popover.show( element );
+						}
+					} );
+				} );
+
+				element.on( 'mouseleave', function( event )
+				{
+					scope.$apply( function()
+					{
+						var popover = getPopover();
+						if ( popover ) {
+							Popover.hideAll();
+						}
+					} );
+				} );
+			}
 		}
 	};
 } );
