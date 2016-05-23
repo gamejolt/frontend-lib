375871ed68405547a8267cfde411eca809ff58df
Yariv Livay <yarivlivay@gmail.com>
2016-05-23 16:03:41 +0000

    Update notification field names to the new ones

--- components/notification/notification-model.js
+++ components/notification/notification-model.js
@@ -7,18 +7,18 @@ angular.module( 'gj.Notification' ).factory( 'Notification', function(
 			angular.extend( this, data );
 		}
 
-		if ( this.subject == 'User' ) {
-			this.subject_model = new ($injector.get( 'User' ))( data.subject_model );
+		if ( this.from == 'User' ) {
+			this.from_resource_model = new ($injector.get( 'User' ))( data.from_resource_model );
 		}
 
-		if ( this.object == 'Game' ) {
-			this.object_model = new ($injector.get( 'Game' ))( data.object_model );
+		if ( this.to == 'Game' ) {
+			this.to_resource_model = new ($injector.get( 'Game' ))( data.to_resource_model );
 		}
-		else if ( this.object == 'User' ) {
-			this.object_model = new ($injector.get( 'User' ))( data.object_model );
+		else if ( this.to == 'User' ) {
+			this.to_resource_model = new ($injector.get( 'User' ))( data.to_resource_model );
 		}
-		else if ( this.object == 'Fireside_Post' ) {
-			this.object_model = new ($injector.get( 'Fireside_Post' ))( data.object_model );
+		else if ( this.to == 'Fireside_Post' ) {
+			this.to_resource_model = new ($injector.get( 'Fireside_Post' ))( data.to_resource_model );
 		}
 
 		this.url = '';
@@ -29,68 +29,68 @@ angular.module( 'gj.Notification' ).factory( 'Notification', function(
 		this.is_game_based = false;
 
 		if ( this.type == Notification.TYPE_COMMENT_ADD ) {
-			this.action_model = new ($injector.get( 'Comment' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'Comment' ))( data.action_resource_model );
 			this.action_label = 'Comment Reply';
 			this.url = undefined;  // Must pull asynchronously when they click on the notification.
 			this.jolticon = 'jolticon-share';
 			this.is_user_based = true;
 		}
 		else if ( this.type == Notification.TYPE_COMMENT_ADD_OBJECT_OWNER ) {
-			this.action_model = new ($injector.get( 'Comment' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'Comment' ))( data.action_resource_model );
 			this.action_label = 'New Comment';
 			this.url = undefined;  // Must pull asynchronously when they click on the notification.
 			this.jolticon = 'jolticon-add-comment';
 			this.is_user_based = true;
 		}
 		else if ( this.type == Notification.TYPE_FORUM_POST_ADD ) {
-			this.action_model = new ($injector.get( 'Forum_Post' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'Forum_Post' ))( data.action_resource_model );
 			this.action_label = 'New Forum Post';
 			this.url = undefined;
 			this.jolticon = 'jolticon-pencil-box';  // TODO: needs-icon
 			this.is_user_based = true;
 		}
 		else if ( this.type == Notification.TYPE_FRIENDSHIP_REQUEST ) {
-			this.action_model = new ($injector.get( 'User_Friendship' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'User_Friendship' ))( data.action_resource_model );
 			this.action_label = 'Friend Request';
-			this.url = $state.href( 'profile.overview', { slug: this.subject_model.slug, id: this.subject_model.id } );
+			this.url = $state.href( 'profile.overview', { slug: this.from_resource_model.slug, id: this.from_resource_model.id } );
 			this.jolticon = 'jolticon-friend-add-1';
 			this.is_user_based = true;
 		}
 		else if ( this.type == Notification.TYPE_FRIENDSHIP_ACCEPT ) {
-			this.action_model = new ($injector.get( 'User_Friendship' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'User_Friendship' ))( data.action_resource_model );
 			this.action_label = 'New Friend';
-			this.url = $state.href( 'profile.overview', { slug: this.subject_model.slug, id: this.subject_model.id } );
+			this.url = $state.href( 'profile.overview', { slug: this.from_resource_model.slug, id: this.from_resource_model.id } );
 			this.jolticon = 'jolticon-friend-add-2';
 			this.is_user_based = true;
 		}
 		else if ( this.type == Notification.TYPE_GAME_RATING_ADD ) {
-			this.action_model = new ($injector.get( 'Game_Rating' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'Game_Rating' ))( data.action_resource_model );
 			this.action_label = 'Game Rating';
-			this.url = this.object_model.getUrl();
+			this.url = this.to_resource_model.getUrl();
 			this.jolticon = 'jolticon-chart';
 			this.is_game_based = true;
 		}
 		else if ( this.type == Notification.TYPE_GAME_FOLLOW ) {
-			this.action_model = new ($injector.get( 'GameLibrary_Game' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'GameLibrary_Game' ))( data.action_resource_model );
 			this.action_label = 'Game Follow';
-			this.url = this.subject_model.url;
+			this.url = this.from_resource_model.url;
 			this.jolticon = 'jolticon-subscribe';
 			this.is_user_based = true;
 		}
 		else if ( this.type == Notification.TYPE_GAME_NEWS_ADD ) {
-			this.action_model = new ($injector.get( 'Game_NewsArticle' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'Game_NewsArticle' ))( data.action_resource_model );
 			this.action_label = 'News Update';
 			this.url = $state.href( 'discover.games.view.news.view', {
-				slug: this.object_model.slug,
-				id: this.object_model.id,
-				articleSlug: this.action_model.slug,
-				articleId: this.action_model.id,
+				slug: this.to_resource_model.slug,
+				id: this.to_resource_model.id,
+				articleSlug: this.action_resource_model.slug,
+				articleId: this.action_resource_model.id,
 			} );
 			this.jolticon = 'jolticon-blog-article';
 			this.is_game_based = true;
 		}
 		else if ( this.type == Notification.TYPE_SELLABLE_SELL ) {
-			this.action_model = new ($injector.get( 'Order_Item' ))( data.action_model );
+			this.action_resource_model = new ($injector.get( 'Order_Item' ))( data.action_resource_model );
 			this.action_label = 'Sale';
 			this.url = $state.href( 'dashboard.main.overview' );
 			this.jolticon = 'jolticon-heart';
@@ -156,10 +156,10 @@ angular.module( 'gj.Notification' ).factory( 'Notification', function(
 		) {
 			var promise = null;
 			if ( this.type == Notification.TYPE_COMMENT_ADD || this.type == Notification.TYPE_COMMENT_ADD_OBJECT_OWNER ) {
-				promise = $injector.get( 'Comment' ).getCommentUrl( this.action_id );
+				promise = $injector.get( 'Comment' ).getCommentUrl( this.action_resource_id );
 			}
 			else if ( this.type == Notification.TYPE_FORUM_POST_ADD ) {
-				promise = $injector.get( 'Forum_Post' ).getPostUrl( this.action_id );
+				promise = $injector.get( 'Forum_Post' ).getPostUrl( this.action_resource_id );
 			}
 
 			promise.then( function( url )
