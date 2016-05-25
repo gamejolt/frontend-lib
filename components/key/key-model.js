88023e851a3ce60706f08de11ec7304e2d7a61c2
Ashley Gwinnell <ashley@forceofhab.it>
2016-05-25 11:57:02 +0100

    Key model.

---
+++ components/key/key-model.js
@@ -0,0 +1,16 @@
+angular.module( 'gj.Key' ).factory( 'Key', function( Model )
+{
+	function Key( data )
+	{
+		if ( data ) {
+			angular.extend( this, data );
+		}
+	}
+
+	Key.prototype.$remove = function( status )
+	{
+		return this.$_remove( '/web/dash/developer/sellables/key-groups/remove-key/' + this.resource_id + '/' + this.sellable_key_group_id + '/' + this.id );
+	};
+
+	return Model.create( Key );
+} );
