87690aca2155334dfddd6006c307b8f834fc015e
David DeCarmine <hworld@holo-world.com>
2016-06-16 16:25:45 +0200

    Video comments now have some different data.

--- components/comment/video/video-model.js
+++ components/comment/video/video-model.js
@@ -1,9 +1,18 @@
-angular.module( 'gj.Comment.Video' ).factory( 'Comment_Video', function( Model, Api )
+angular.module( 'gj.Comment.Video' ).factory( 'Comment_Video', function( $injector, Model, Api, Game )
 {
 	function Comment_Video( data )
 	{
 		if ( data ) {
 			angular.extend( this, data );
+
+			if ( data.comment ) {
+				var Comment = $injector.get( 'Comment' );
+				this.comment = new Comment( data.comment );
+			}
+
+			if ( data.game ) {
+				this.game = new Game( data.game );
+			}
 		}
 	}
 
