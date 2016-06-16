3eec41308a5cb13ac3282cb330e1df92cec69af9
David DeCarmine <hworld@holo-world.com>
2016-06-16 16:25:58 +0200

    Got most things working within the video comment lightbox.

--- components/comment/video/lightbox/lightbox-directive.js
+++ components/comment/video/lightbox/lightbox-directive.js
@@ -4,19 +4,53 @@ angular.module( 'gj.Comment.Video.Lightbox' ).component( 'gjCommentVideoLightbox
 		onClose: '&',
 	},
 	templateUrl: '/lib/gj-lib-client/components/comment/video/lightbox/lightbox.html',
-	controller: function( $scope, $timeout, Screen, hotkeys )
+	controller: function( $scope, $timeout, $location, Environment, Screen, Comment, hotkeys )
 	{
 		var _this = this;
+		this.screen = Screen;
 
 		this.calcMaxDimensions = calcMaxDimensions;
+		this.toggleVote = toggleVote;
+		this.reply = reply;
 
 		this.calcMaxDimensions();
 
 		function calcMaxDimensions()
 		{
-			this.maxWidth = (Screen.windowWidth * 0.8);
-			this.maxHeight = Screen.windowHeight - (80 * 2);
+			if ( Screen.isXs ) {
+				this.maxWidth = Screen.width;
+				this.maxHeight = Screen.height;
+			}
+			else {
+				this.maxWidth = (Screen.width * 0.8);
+				this.maxHeight = Screen.height - (80 * 2);
+			}
 		};
+
+		function toggleVote()
+		{
+			// If adding a vote.
+			if ( !this.video.comment.userVote ) {
+				this.video.comment.$like();
+			}
+			// If removing a vote.
+			else {
+				this.video.comment.$removeLike();
+			}
+		}
+
+		function reply()
+		{
+			Comment.getCommentUrl( this.video.comment.id ).then( function( url )
+			{
+				var search = Environment.baseUrl;
+				if ( url.search( search ) === 0 ) {
+					url = url.replace( search, '' );
+				}
+				$location.url( url );
+				_this.onClose();
+			} );
+		}
 
 		Screen.setResizeSpy( $scope, function()
 		{
