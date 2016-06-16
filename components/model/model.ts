d13cbba50f9c1f043e003a1988b0d4458b268a81
David DeCarmine <hworld@holo-world.com>
2016-06-17 00:41:48 +0200

    Update Model to not export a const module.

--- components/model/model.ts
+++ components/model/model.ts
@@ -1,7 +1,7 @@
 import { ModelFactory } from './model-service';
 export { Model } from './model-service';
 
-export const ModelModule = angular.module( 'gj.Model', [] )
+export default angular.module( 'gj.Model', [] )
 .factory( 'Model', ModelFactory )
 .name
 ;
