33268ee36e9790d2f9647ec4f74f8b0a672e5fd6
David DeCarmine <hworld@holo-world.com>
2016-06-18 01:30:04 +0200

    Fix remove method in youtube channel model.

--- components/youtube/channel/channel-model.ts
+++ components/youtube/channel/channel-model.ts
@@ -21,27 +21,6 @@ export class Youtube_Channel extends Model
 
 	$remove()
 	{
-		return this.$_remove( '/web/dash/linked-accounts/unlink/youtube-channel/' + this.channel_id, 'user' );
+		return this.$_remove( '/web/dash/linked-accounts/unlink/youtube-channel/' + this.channel_id );
 	}
 }
-
-// angular.module( 'gj.Youtube.Channel' ).factory( 'Youtube_Channel', function( Model, User, Api )
-// {
-// 	function Youtube_Channel( data )
-// 	{
-// 		if ( data ) {
-// 			angular.extend( this, data );
-// 		}
-
-// 		if ( this.user ) {
-// 			this.user = new User( this.user );
-// 		}
-// 	}
-
-// 	Youtube_Channel.prototype.$remove = function()
-// 	{
-// 		return this.$_remove( '/web/dash/linked-accounts/unlink/youtube-channel/' + this.channel_id, 'user' );
-// 	};
-
-// 	return Model.create( Youtube_Channel );
-// } );
