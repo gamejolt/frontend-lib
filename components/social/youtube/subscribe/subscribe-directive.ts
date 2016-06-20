993a689c96dc99c1d7b04c897613840ef50a2c61
David DeCarmine <hworld@holo-world.com>
2016-06-20 17:28:45 +0200

    Fix youtube subscribe button. It was broken for channel IDs, which is what we use.

--- components/social/youtube/subscribe/subscribe-directive.ts
+++ components/social/youtube/subscribe/subscribe-directive.ts
@@ -4,7 +4,7 @@ import { Youtube_Sdk } from './../sdk/sdk-service';
 @Component({
 	selector: 'gj-social-youtube-subscribe',
 	template: `
-	<div class="g-ytsubscribe" data-channel="{{ ::$ctrl.channel }}" data-layout="{{ ::$ctrl.layout }}" data-theme="{{ ::$ctrl.theme }}" data-count="default"></div>
+	<div class="g-ytsubscribe" data-channelid="{{ ::$ctrl.channel }}" data-layout="{{ ::$ctrl.layout }}" data-theme="{{ ::$ctrl.theme }}" data-count="default"></div>
 	`,
 })
 export class SubscribeComponent
