2ea67f33286bbf05f729928cb3e633a19734f554
David DeCarmine <hworld@holo-world.com>
2016-06-23 20:41:23 +0200

    Fix model code.

--- components/model/model.ts
+++ components/model/model.ts
@@ -1,12 +1,6 @@
 import { ModelFactory } from './model-service';
-<<<<<<< HEAD
-
-export const ModelModule = angular.module( 'gj.Model', [] )
-=======
-export { Model } from './model-service';
 
 export default angular.module( 'gj.Model', [] )
->>>>>>> master
 .factory( 'Model', ModelFactory )
 .name
 ;
