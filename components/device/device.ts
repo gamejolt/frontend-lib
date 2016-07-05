04b2f97c79f077bf611aab20b2be24831ead1668
David DeCarmine <hworld@holo-world.com>
2016-07-05 13:41:31 +0200

    Revert "Convert Device to TS."
    This reverts commit f8cfb21d9d1293af4d15525daed15de88f62920d.

--- components/device/device.ts
+++
@@ -1,7 +0,0 @@
-import { provide } from 'ng-metadata/core';
-import { Device } from './device-service';
-
-export default angular.module( 'gj.Device', [
-] )
-.service( ...provide( 'Device', { useClass: Device } ) )
-.name;
