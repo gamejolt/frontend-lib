bada3f830b96e364ac223aed6619148c19f00a17
David DeCarmine <hworld@holo-world.com>
2016-07-25 21:10:38 +0800

    Clean out game build file and game build param models.

--- components/game/build/file/file-model.js
+++ components/game/build/file/file-model.js
@@ -7,20 +7,5 @@ angular.module( 'gj.Game.Build.File' ).factory( 'Game_Build_File', function( Mod
 		}
 	}
 
-	Game_Build_File.prototype.$save = function()
-	{
-		// Add only.
-		var params = [ this.game_id, this.game_package_id, this.game_release_id, this.game_build_id ];
-		if ( !this.id ) {
-			return this.$_save( '/web/dash/developer/games/builds/files/save/' + params.join( '/' ), 'gameBuildFile', { file: this.file } );
-		}
-	};
-
-	Game_Build_File.prototype.$remove = function( gameId, packageId, releaseId )
-	{
-		var params = [ gameId, packageId, releaseId, this.game_build_id, this.id ];
-		return this.$_remove( '/web/dash/developer/games/builds/files/remove/' + params.join( '/' ) );
-	};
-
 	return Model.create( Game_Build_File );
 } );
