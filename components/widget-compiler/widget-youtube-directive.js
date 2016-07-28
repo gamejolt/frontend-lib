13cc36427bc19daff94c6477a1b13a5213a653d0
David DeCarmine <hworld@holo-world.com>
2016-07-28 15:36:05 +0800

    Change widget compiler to use video-embed directive.

--- components/widget-compiler/widget-youtube-directive.js
+++ components/widget-compiler/widget-youtube-directive.js
@@ -1,28 +1,10 @@
-angular.module( 'gj.WidgetCompiler' ).directive( 'gjWidgetYoutube', function( $sce, $rootScope, $timeout, Ruler, Screen )
+angular.module( 'gj.WidgetCompiler' ).directive( 'gjWidgetYoutube', function()
 {
 	return {
 		restrict: 'E',
-		template: '<iframe nwdisable nwfaketop type="text/html" frameborder="0" width="100%" height="{{videoHeight}}" ng-src="{{embedSrc}}"></iframe>',
+		template: '<gj-video-embed video-provider="youtube" video-id="videoId"></gj-video-embed>',
 		scope: {
 			videoId: '=gjWidgetYoutubeVideoId'
 		},
-		link: function( scope, element )
-		{
-			scope.embedSrc = $sce.trustAsResourceUrl( 'https://www.youtube.com/embed/' + scope.videoId );
-
-			function recalculateDimensions()
-			{
-				scope.videoHeight = Ruler.width( element[0].getElementsByTagName( 'iframe' )[0] ) * 0.5625;
-			};
-
-			// Wait to be compiled before recalculating the first time.
-			$timeout( function()
-			{
-				recalculateDimensions();
-			} );
-
-			// Also recalc every resize.
-			Screen.setResizeSpy( scope, recalculateDimensions );
-		}
 	};
 } );
