482ce9bd88534ed0a4542bcc104e65cefdbbdf7e
David DeCarmine <hworld@holo-world.com>
2016-12-23 14:11:06 -0700

    New functionality in the pricing model to get pricing info out of a pricings array.

--- components/sellable/pricing/pricing-model.js
+++ components/sellable/pricing/pricing-model.js
@@ -7,5 +7,24 @@ angular.module( 'gj.Sellable.Pricing' ).factory( 'Sellable_Pricing', function( M
 		}
 	}
 
+	Sellable_Pricing.getOriginalPricing = function( pricings )
+	{
+		if ( Array.isArray( pricings ) ) {
+			if ( pricings[0].promotional ) {
+				return pricings[1];
+			}
+			return pricings[0];
+		}
+		return undefined;
+	};
+
+	Sellable_Pricing.getPromotionalPricing = function( pricings )
+	{
+		if ( Array.isArray( pricings ) && pricings[0].promotional ) {
+			return pricings[0];
+		}
+		return undefined;
+	};
+
 	return Model.create( Sellable_Pricing );
 } );
