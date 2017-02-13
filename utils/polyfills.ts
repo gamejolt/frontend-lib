37d04d7812ff4196ae015081edd6002cfa76e3eb
David DeCarmine <hworld@holo-world.com>
2017-02-13 14:44:49 -0700

    Add polyfills.

---
+++ utils/polyfills.ts
@@ -0,0 +1,11 @@
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/array';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/es6/promise';
+import 'core-js/modules/es7.object.values';
+import 'reflect-metadata';
