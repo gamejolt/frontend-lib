1550584556d2b8d0416b1b0af14ebb42ee20e987
David DeCarmine <hworld@holo-world.com>
2017-02-14 13:22:28 -0700

    Add widget compiler ng module

--- components/widget-compiler/widget-compiler.module.ts
+++ components/widget-compiler/widget-compiler.module.ts
@@ -1,21 +1,10 @@
-// import { provide } from 'ng-metadata/core';
+import { NgModule } from 'ng-metadata/core';
+import { makeComponentProvider } from '../../vue/angular-link';
+import { AppWidgetCompiler } from './widget-compiler';
 
-// import { WidgetCompiler } from './widget-compiler-service';
-// import { WidgetCompilerBindComponent } from './widget-compiler-bind.directive';
-// import { WidgetCompilerWidgetYoutube } from './widget-youtube.service';
-// import { WidgetCompilerWidgetVimeo } from './widget-vimeo.service';
-// import { WidgetCompilerWidgetSoundcloud } from './widget-soundcloud.service';
-// import { WidgetCompilerWidgetYoutubeComponent } from './widget-youtube.component';
-// import { WidgetCompilerWidgetVimeoComponent } from './widget-vimeo.component';
-// import { WidgetCompilerWidgetSoundcloudComponent } from './widget-soundcloud.component';
-
-// export default angular.module( 'gj.WidgetCompiler', [] )
-// .service( ...provide( 'WidgetCompiler', { useClass: WidgetCompiler } ) )
-// .directive( ...provide( WidgetCompilerBindComponent ) )
-// .service( ...provide( 'WidgetCompilerWidgetYoutube', { useClass: WidgetCompilerWidgetYoutube } ) )
-// .directive( ...provide( WidgetCompilerWidgetYoutubeComponent ) )
-// .service( ...provide( 'WidgetCompilerWidgetVimeo', { useClass: WidgetCompilerWidgetVimeo } ) )
-// .directive( ...provide( WidgetCompilerWidgetVimeoComponent ) )
-// .service( ...provide( 'WidgetCompilerWidgetSoundcloud', { useClass: WidgetCompilerWidgetSoundcloud } ) )
-// .directive( ...provide( WidgetCompilerWidgetSoundcloudComponent ) )
-// .name;
+@NgModule({
+	declarations: [
+		makeComponentProvider( AppWidgetCompiler ),
+	],
+})
+export class WidgetCompilerModule { }
