f7e1fe598fb9730079cf92dd8610685c3c4b84b1
David DeCarmine <hworld@holo-world.com>
2017-02-14 17:05:38 -0700

    New tab list component.

---
+++ components/nav/tab-list/tab-list.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from 'ng-metadata/core';
+import { makeComponentProvider } from '../../../vue/angular-link';
+import { AppNavTabList } from './tab-list';
+
+@NgModule({
+	declarations: [
+		makeComponentProvider( AppNavTabList ),
+	],
+})
+export class NavTabListModule { }
---
+++ components/nav/tab-list/tab-list.ts
@@ -0,0 +1,12 @@
+import * as Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import * as View from '!view?-scoped!./tab-list.html?style=./tab-list.styl';
+
+@View
+@Component({
+	name: 'nav-tab-list',
+})
+export class AppNavTabList extends Vue
+{
+	@Prop( Boolean ) center?: boolean;
+}
