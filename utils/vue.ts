56267dde399ed948911483076a57df7778950f89
David DeCarmine <hworld@holo-world.com>
2017-02-15 14:53:27 -0700

    New function to wrap a static service in Vue to make it reactive.

--- utils/vue.ts
+++ utils/vue.ts
@@ -12,3 +12,17 @@ export function findVueParent( component: Vue, parentType: typeof Vue )
 
 	return undefined;
 }
+
+export function makeObservableService<T>( service: T ): T
+{
+	// We have to loop through all properties of the service and make them reactive.
+	// We should only do it once.
+	if ( !(service as any).__gjObservable__ ) {
+		for ( const k in service ) {
+			(Vue as any).util.defineReactive( service, k, service[ k ] );
+		}
+		(service as any).__gjObservable__ = true;
+	}
+
+	return service;
+}
