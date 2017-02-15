31ed0caa60c1bcf51a6a9a246f76324957dd83b7
David DeCarmine <hworld@holo-world.com>
2017-02-14 17:05:24 -0700

    Angular link for vue now works when no props. It also works with slots/transclusion.

--- vue/angular-link.ts
+++ vue/angular-link.ts
@@ -1,5 +1,5 @@
 import * as Vue from 'vue';
-import { Component, Inject, OnChanges, SimpleChanges, OnDestroy } from 'ng-metadata/core';
+import { Component, Inject, OnChanges, SimpleChanges, OnDestroy, AfterViewInit } from 'ng-metadata/core';
 import { kebabCase } from '../utils/string';
 
 export function makeComponentProvider( component: typeof Vue )
@@ -15,8 +15,11 @@ export function makeComponentProvider( component: typeof Vue )
 
 	@Component( {
 		selector: `gj-${kebabName}`,
-		inputs: Object.keys( options.props ),
-		template: '<div></div>',
+		inputs: options.props ? Object.keys( options.props ) : undefined,
+		template: '<div ng-transclude></div>',
+		legacy: {
+			transclude: true,
+		}
 	})
 	class WrappedComponent implements OnChanges, OnDestroy
 	{
@@ -29,7 +32,7 @@ export function makeComponentProvider( component: typeof Vue )
 		{
 			this.el = this.$element[ 0 ];
 
-			const rootElement = createVueElement( options );
+			const rootElement = createVueElement( this.el, options );
 			rootElement.el = this.el.children[ 0 ];
 
 			this.vueElement = new Vue( rootElement );
@@ -41,7 +44,7 @@ export function makeComponentProvider( component: typeof Vue )
 		{
 			for ( const key in changes ) {
 				if ( typeof changes[ key ].currentValue !== 'undefined' ) {
-					(this.vueElement as any)[ key ] = changes[ key ].currentValue;
+					( this.vueElement as any )[ key ] = changes[ key ].currentValue;
 				}
 			}
 		}
@@ -57,10 +60,13 @@ export function makeComponentProvider( component: typeof Vue )
 	return WrappedComponent;
 }
 
-function createVueElement( componentDefinition: any )
+function createVueElement( el: HTMLElement, componentDefinition: any )
 {
 	componentDefinition = Object.assign( {}, componentDefinition );
-	const props: string[] = componentDefinition.props;
+	const props: string[] = componentDefinition.props || {};
+
+	// clone hack due to IE compatibility
+	const elOriginalChildren = el.cloneNode( true ).childNodes;
 
 	const rootElement: Vue.ComponentOptions<Vue> = {
 		props,
@@ -68,7 +74,7 @@ function createVueElement( componentDefinition: any )
 			reactiveProps( this: any )
 			{
 				const reactivePropsList: any = {};
-				Object.keys( props ).forEach( ( prop ) => reactivePropsList[ prop ] = this[ prop ] );
+				Object.keys( props ).forEach(( prop ) => reactivePropsList[ prop ] = this[ prop ] );
 				return reactivePropsList;
 			}
 		},
@@ -78,9 +84,60 @@ function createVueElement( componentDefinition: any )
 				props: this.reactiveProps
 			};
 
-			return createElement( componentDefinition, data );
+			const slots = getSlots( elOriginalChildren, createElement );
+
+			return createElement(
+				componentDefinition,
+				data,
+				slots,
+			);
 		}
 	};
 
 	return rootElement;
+}
+
+export function getAttributes( child: Node )
+{
+	const attributes: { [k: string]: string | null | undefined } = {};
+
+	for ( let i = 0; i < child.attributes.length; ++i ) {
+		const attribute = child.attributes[ i ];
+		attributes[ attribute.nodeName === 'vue-slot' ? 'slot' : attribute.nodeName ] = attribute.nodeValue;
+	}
+
+	return attributes;
+}
+
+export function getSlots( children: NodeList, createElement: Vue.CreateElement )
+{
+	const slots = [];
+
+	for ( let i = 0; i < children.length; ++i ) {
+		const child = children[ i ] as HTMLElement;
+
+		if ( child.nodeName === '#text' ) {
+			if ( child.nodeValue!.trim() ) {
+				slots.push( createElement( 'span', child.nodeValue as string ) );
+			}
+		}
+		else {
+			const attributes = getAttributes( child );
+			const elementOptions: any = {
+				attrs: attributes,
+				domProps: {
+					innerHTML: child.innerHTML,
+				}
+			};
+
+			if ( attributes['slot'] ) {
+				elementOptions.slot = attributes['slot'];
+				attributes['slot'] = undefined;
+			}
+
+			slots.push( createElement( child.tagName, elementOptions ) );
+		}
+	}
+
+	return slots;
 }
