2628c1240cc563616052fc74c309f25eab991d23
David DeCarmine <hworld@holo-world.com>
2017-02-21 23:16:59 -0700

    New utility function to trigger an event.

---
+++ utils/event.ts
@@ -0,0 +1,21 @@
+
+// Polyfill CustomEvent for stupid <IE11
+if ( typeof (window as any).CustomEvent !== 'function' ) {
+
+	function _CustomEvent( event: any, params: any )
+	{
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		const evt = document.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	}
+
+	_CustomEvent.prototype = (window as any).Event.prototype;
+	(window as any).CustomEvent = _CustomEvent;
+}
+
+export function triggerEvent( el: HTMLElement, eventName: string, data: any )
+{
+	const event = new CustomEvent( eventName, { detail: data } );
+	el.dispatchEvent( event );
+}
