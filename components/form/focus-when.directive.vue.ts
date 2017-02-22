07f1fd2bb453f6d85f5b0e6c90a2212d71b6a692
David DeCarmine <hworld@holo-world.com>
2017-02-21 23:19:44 -0700

    New vue directive to focus input.

---
+++ components/form/focus-when.directive.vue.ts
@@ -0,0 +1,14 @@
+import * as Vue from 'vue';
+
+export const AppFocusWhen: Vue.DirectiveOptions = {
+	inserted( el: HTMLElement )
+	{
+		el.focus();
+	},
+	update( el: HTMLElement, binding )
+	{
+		if ( binding.value && binding.value !== binding.oldValue ) {
+			el.focus();
+		}
+	},
+};
