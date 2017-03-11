6b706ba28bca6b42cf1007592384f2b0b953df06
David DeCarmine <hworld@holo-world.com>
2017-03-11 13:34:32 -0700

    Remove translate dependency from comment widget.

--- components/comment/widget/widget.js
+++ components/comment/widget/widget.js
@@ -1,4 +1,4 @@
-angular.module( 'gj.Comment.Widget', [ 'gj.Pagination', 'gj.Form.MarkdownEditor', 'gj.FadeCollapse', 'gj.Translation', 'gj.Translate', 'gj.Report.Modal' ] );
+angular.module( 'gj.Comment.Widget', [ 'gj.Pagination', 'gj.Form.MarkdownEditor', 'gj.FadeCollapse', 'gj.Translation', 'gj.Report.Modal' ] );
 
 require( './add-form-directive' );
 require( './comment-directive' );
