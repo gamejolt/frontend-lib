faf680d1fec98c839eeafc20ae1a69e6a903396d
David DeCarmine <hworld@holo-world.com>
2017-03-10 18:39:03 -0700

    Add stringSort helper

--- utils/array.ts
+++ utils/array.ts
@@ -5,3 +5,17 @@ export function arrayUnique( values: any[] )
 		return values.indexOf( value ) === index;
 	} );
 }
+
+export function stringSort( a: string, b: string )
+{
+	a = a.toLowerCase();
+	b = b.toLowerCase();
+
+	if ( a < b ) {
+		return -1;
+	}
+	else if ( a > b ) {
+		return 1;
+	}
+	return 0;
+}
