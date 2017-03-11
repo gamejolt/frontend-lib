79c1f0ea688924e2af8e5a7afa71a5b0ba881666
David DeCarmine <hworld@holo-world.com>
2017-03-11 13:00:15 -0700

    New vue direct to track click events.

---
+++ components/analytics/track-event.directive.vue.ts
@@ -0,0 +1,15 @@
+import * as Vue from 'vue';
+import { Analytics } from './analytics.service';
+
+export const AppTrackEvent: Vue.DirectiveOptions = {
+	bind( el, binding )
+	{
+		el.addEventListener( 'click', () =>
+		{
+			if ( binding.value ) {
+				const pieces = binding.value.split( ':' );
+				Analytics.trackEvent( pieces[0], pieces[1], pieces[2], pieces[3] );
+			}
+		} );
+	},
+};
