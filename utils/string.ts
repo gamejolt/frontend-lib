8d405f275a302c8aba64ebea555a38b0b049a707
David DeCarmine <hworld@holo-world.com>
2017-03-10 18:39:11 -0700

    Add fuzzysearch helper

--- utils/string.ts
+++ utils/string.ts
@@ -5,3 +5,26 @@ export function kebabCase( name: string )
 	return name.replace( KebabCaseRegex, ( letter, pos ) =>
 		(pos ? '-' : '') + letter.toLowerCase() );
 }
+
+// Pulled from: https://github.com/bevacqua/fuzzysearch
+export function fuzzysearch( query: string, text: string )
+{
+	const tlen = text.length;
+	const qlen = query.length;
+	if ( qlen > tlen ) {
+		return false;
+	}
+	if ( qlen === tlen && query === text ) {
+		return true;
+	}
+	outer: for ( let i = 0, j = 0; i < qlen; i++ ) {
+		const qch = query.charCodeAt( i );
+		while ( j < tlen ) {
+			if ( text.charCodeAt( j++ ) === qch ) {
+				continue outer;
+			}
+		}
+		return false;
+	}
+	return true;
+}
