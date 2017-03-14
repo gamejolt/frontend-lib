6a7a8c73c3ea3b277bc1f39e6157a04ba6da0f2b
David DeCarmine <hworld@holo-world.com>
2017-03-13 18:44:19 -0600

    New global event bus service.

---
+++ components/event-bus/event-bus.service.ts
@@ -0,0 +1,32 @@
+import * as Vue from 'vue';
+import { getProvider } from '../../utils/utils';
+
+const eventBus = new Vue();
+
+export class EventBus
+{
+	static emit( event: string, ...args: any[] )
+	{
+		if ( GJ_IS_ANGULAR ) {
+			const $rootScope = getProvider<any>( '$rootScope' );
+			$rootScope.$emit( event, ...args );
+		}
+
+		eventBus.$emit( event, ...args );
+	}
+
+	static on( event: string, callback: Function )
+	{
+		eventBus.$on( event, callback );
+	}
+
+	static once( event: string, callback: Function )
+	{
+		eventBus.$once( event, callback );
+	}
+
+	static off( event?: string, callback?: Function )
+	{
+		eventBus.$off( event, callback );
+	}
+}
