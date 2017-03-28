1c3820979842f978a14ca1d18f5f4dad25f9492d
David DeCarmine <hworld@holo-world.com>
2017-03-27 20:42:57 -0600

    Added video comment components

--- components/comment/video/thumbnail/thumbnail.ts
+++ components/comment/video/thumbnail/thumbnail.ts
@@ -1,6 +1,32 @@
-import { provide } from 'ng-metadata/core';
-import { ThumbnailComponent } from './thumbnail-directive';
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import * as View from '!view!./thumbnail.html?style=./thumbnail.styl';
 
-export default angular.module( 'gj.Comment.Video.Thumbnail', [] )
-.directive( ...provide( ThumbnailComponent ) )
-.name;
+import { CommentVideo } from '../video-model';
+import { AppJolticon } from '../../../../vue/components/jolticon/jolticon';
+import { AppTrackEvent } from '../../../analytics/track-event.directive.vue';
+import { AppTooltip } from '../../../tooltip/tooltip';
+import { AppGameThumbnailImg } from '../../../game/thumbnail-img/thumbnail-img';
+import { AppCommentVideoLightbox } from '../lightbox/lightbox';
+
+@View
+@Component({
+	name: 'comment-video-thumbnail',
+	components: {
+		AppJolticon,
+		AppGameThumbnailImg,
+		AppCommentVideoLightbox,
+	},
+	directives: {
+		AppTrackEvent,
+		AppTooltip,
+	},
+})
+export class AppCommentVideoThumbnail extends Vue
+{
+	@Prop( CommentVideo ) video: CommentVideo;
+	@Prop( Boolean ) showUser?: boolean;
+	@Prop( Boolean ) showGame?: boolean;
+
+	isLightboxActive = false;
+}
