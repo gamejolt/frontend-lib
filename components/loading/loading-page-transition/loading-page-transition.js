95cbea1b61e0d54f13432f9c8e343ebbfd06d0ac
David DeCarmine <hworld@holo-world.com>
2017-04-03 17:14:34 -0600

    Convert loading-bar to vue

--- components/loading/loading-page-transition/loading-page-transition.js
+++
@@ -1,7 +0,0 @@
-angular.module( 'gj.Loading.LoadingPageTransition', [ 'angular-loading-bar' ] ).config( function( cfpLoadingBarProvider )
-{
-	cfpLoadingBarProvider.latencyThreshold = 200;
-	cfpLoadingBarProvider.includeSpinner = false;
-} );
-
-require( './loading-page-transition-directive' );
