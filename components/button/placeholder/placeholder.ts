38f1394b024203591cfc24c56e95f251e96f4bab
David DeCarmine <hworld@holo-world.com>
2017-04-05 13:18:07 -0600

    some better placeholder styles added a button placeholder

---
+++ components/button/placeholder/placeholder.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import * as View from '!view!./placeholder.html?style=./placeholder.styl';
+
+@View
+@Component({})
+export class AppButtonPlaceholder extends Vue
+{
+	@Prop( Boolean ) sparse?: boolean;
+}
