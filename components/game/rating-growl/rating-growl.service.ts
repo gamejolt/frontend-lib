9d46b5ed121eb3c61836badfb1aab753f8b57bcf
David DeCarmine <hworld@holo-world.com>
2017-04-17 18:09:51 -0600

    added rating growl to frontend lib

---
+++ components/game/rating-growl/rating-growl.service.ts
@@ -0,0 +1,41 @@
+import { AppGameRatingGrowl } from './rating-growl';
+import { Game } from '../game.model';
+import { appStore } from '../../../vue/services/app/app-store';
+import { Api } from '../../api/api.service';
+import { Growls } from '../../growls/growls.service';
+
+export class GameRatingGrowl
+{
+	static async show( game: Game )
+	{
+		const app = appStore.state!;
+
+		// Don't show when not logged in.
+		if ( !app.user ) {
+			return;
+		}
+
+		// Don't show if ratings are disabled for the game.
+		if ( !game.ratings_enabled || !game.can_user_rate ) {
+			return;
+		}
+
+		// Don't show when this is the developer of the game.
+		if ( app.user.id === game.developer.id ) {
+			return;
+		}
+
+		const payload = await Api.sendRequest( '/web/discover/games/ratings/get-user-rating/' + game.id, null, { detach: true } );
+
+		// If there is a rating for this user already, don't show the growl.
+		if ( payload.rating ) {
+			return;
+		}
+
+		Growls.info( {
+			sticky: true,
+			component: AppGameRatingGrowl,
+			props: { game },
+		} );
+	}
+}
---
+++ components/game/rating-growl/rating-growl.ts
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import * as View from '!view!./rating-growl.html';
+
+import { AppRatingWidget } from '../../../../../app/components/rating/widget/widget';
+import { Game } from '../game.model';
+import { EventBus } from '../../event-bus/event-bus.service';
+
+
+@View
+@Component({
+	components: {
+		AppRatingWidget,
+	}
+})
+export class AppGameRatingGrowl extends Vue
+{
+	@Prop( Object ) game: Game;
+
+	mounted()
+	{
+		// Close the modal as soon as they rate the game. We set up on $on event
+		// so that we get notified even if they rate the game from the game page
+		// and not the modal.
+		EventBus.on( 'GameRating.changed', ( gameId: number ) =>
+		{
+			if ( gameId === this.game.id ) {
+				this.$emit( 'close' );
+			}
+		} );
+	}
+}
