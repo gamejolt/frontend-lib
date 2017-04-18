3c284658a07a57e5a8447d21b1a45f5fbfa5f2ab
David DeCarmine <hworld@holo-world.com>
2017-04-17 18:09:31 -0600

    added minbar to frontend lib

---
+++ components/minbar/minbar.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import * as View from '!view!./minbar.html?style=./minbar.styl';
+
+import { Screen } from '../screen/screen-service';
+import { AppTooltip } from '../tooltip/tooltip';
+import { makeObservableService } from '../../utils/vue';
+import { Minbar, MinbarItem } from './minbar.service';
+
+@View
+@Component({
+	directives: {
+		AppTooltip,
+	},
+})
+export class AppMinbar extends Vue
+{
+	Minbar = makeObservableService( Minbar );
+	Screen = makeObservableService( Screen );
+
+	onItemClick( item: MinbarItem )
+	{
+		if ( item.onClick ) {
+			item.onClick();
+		}
+	}
+}
