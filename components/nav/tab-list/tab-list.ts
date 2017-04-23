4ac833084251dbcf200081791fb599263ed462a7
David DeCarmine <hworld@holo-world.com>
2017-04-22 21:46:03 -0600

    Nav tab list now supports meta and input slots

--- components/nav/tab-list/tab-list.ts
+++ components/nav/tab-list/tab-list.ts
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import * as View from '!view?-scoped!./tab-list.html?style=./tab-list.styl';
 
+import { Screen } from '../../screen/screen-service';
+import { makeObservableService } from '../../../utils/vue';
+
 @View
 @Component({})
 export class AppNavTabList extends Vue
 {
 	@Prop( Boolean ) center?: boolean;
-
 	@Prop( Number ) slotId: number;
+
+	Screen = makeObservableService( Screen );
 }
