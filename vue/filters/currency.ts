1aeafe24065e2fb8f44e825d2dfb18487329f7b7
David DeCarmine <hworld@holo-world.com>
2017-04-25 10:13:57 -0600

    Fix up currency rounding when decimal is 0

--- vue/filters/currency.ts
+++ vue/filters/currency.ts
@@ -1,8 +1,8 @@
 export function currency( amount: number, currencySymbol = '$', decimal = 2 ): string
 {
 	// No fraction/decimal.
-	if ( amount % 100 === 0 ) {
-		return currencySymbol + (amount / 100).toFixed( 0 );
+	if ( amount % 100 === 0 || decimal === 0 ) {
+		return currencySymbol + Math.floor( amount / 100 );
 	}
 
 	return currencySymbol + (amount / 100).toFixed( decimal );
