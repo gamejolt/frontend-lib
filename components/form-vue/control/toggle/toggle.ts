0f939166d5be2a6989836f94d242ed4bdd9ccccb
David DeCarmine <hworld@holo-world.com>
2017-06-20 11:33:39 -0600

    remove labels for toggle switches

--- components/form-vue/control/toggle/toggle.ts
+++ components/form-vue/control/toggle/toggle.ts
@@ -6,19 +6,9 @@ import { BaseFormControl } from '../base';
 @View
 @Component({})
 export class AppFormControlToggle extends BaseFormControl {
-	@Prop(String) onLabel?: string;
-	@Prop(String) offLabel?: string;
 	@Prop(Boolean) disabled?: boolean;
 
 	value: boolean = false;
-
-	get _onLabel() {
-		return this.onLabel || this.$gettext('On');
-	}
-
-	get _offLabel() {
-		return this.offLabel || this.$gettext('Off');
-	}
 
 	toggle() {
 		if (this.disabled) {
