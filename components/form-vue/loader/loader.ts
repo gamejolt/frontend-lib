632a512dd260532bb9fb9414d2385923669d2d3e
Yariv Livay <yarivlivay@gmail.com>
2017-07-16 22:51:53 +0300

    Make app form loader reset when reloaded

--- components/form-vue/loader/loader.ts
+++ components/form-vue/loader/loader.ts
@@ -13,19 +13,21 @@ import { AppLoading } from '../../../vue/components/loading/loading';
 })
 export class AppFormLoader extends Vue {
 	@Prop(String) url?: string;
-	@Prop(Promise) load?: Promise<any>;
 	@Prop(Object) data?: any;
 
 	isLoaded = false;
 
-	async mounted() {
+	mounted() {
+		this.load();
+	}
+
+	async load() {
+		this.isLoaded = false;
 		let payload;
 		if (this.url) {
 			payload = await Api.sendRequest(this.url, this.data || undefined, {
 				detach: true,
 			});
-		} else if (this.load) {
-			payload = await this.load;
 		}
 
 		this.isLoaded = true;
