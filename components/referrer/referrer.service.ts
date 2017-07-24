cf998cd60a049c84035438e8f3cbbebd495ac18b
David DeCarmine <hworld@holo-world.com>
2017-07-24 13:37:59 -0600

    Port the Referrer service

--- components/referrer/referrer.service.ts
+++ components/referrer/referrer.service.ts
@@ -1,75 +1,60 @@
+import VueRouter from 'vue-router';
+import { EventBus } from '../event-bus/event-bus.service';
+
 /**
- * Since we're in a single page app, the referrer doesn't get reset on every page change.
- * To be able to pull the correct referrer we need to spoof it by updating on every state change.
- * The initial referrer from the Document should be correct when we first hit the page.
- * If it's "null" then there was no referrer when hitting the initial page.
+ * Since we're in a single page app, the referrer doesn't get reset on every
+ * page change. To be able to pull the correct referrer we need to spoof it by
+ * updating on every state change. The initial referrer from the Document should
+ * be correct when we first hit the page. If it's "null" then there was no
+ * referrer when hitting the initial page.
  */
 export class Referrer {
 	private static isInitialized = false;
 
 	/**
-	 * We will set this to false after the first page change.
-	 * We don't artifically track new referrers until after the first page has passed.
+	 * We will set this to false after the first page change. We don't
+	 * artifically track new referrers until after the first page has passed.
 	 */
 	private static firstPass = true;
 
 	/**
-	 * After every location change we store the current URL.
-	 * We can use this value as the referrer when switching to the next page.
+	 * After every location change we store the current URL. We can use this
+	 * value as the referrer when switching to the next page.
 	 */
-	private static currentUrl?: string;
+	private static url?: string;
 
-	private static prev?: string;
-	private static referrer?: string;
+	private static _referrer?: string;
 
-	static init() {
+	static init(router: VueRouter) {
 		this.isInitialized = true;
 
 		if (!GJ_IS_SSR && window.document.referrer) {
-			this.referrer = window.document.referrer;
+			this._referrer = window.document.referrer;
 		}
+
+		router.beforeEach((_to, _from, next) => {
+			// Don't track until we've tracked on full page view.
+			if (this.firstPass) {
+				return next();
+			}
+
+			this._referrer = this.url;
+			next();
+		});
+
+		EventBus.on('routeChangeAfter', () => {
+			// We have finished the first state change.
+			// We will now begin tracking new referrers.
+			this.firstPass = false;
+			this.url = window.location.href;
+		});
 	}
 
-	// static initAngular( $rootScope: any )
-	// {
-	// 	if ( !GJ_IS_ANGULAR ) {
-	// 		return;
-	// 	}
-
-	// 	this.init();
-
-	// 	$rootScope.$on( '$stateChangeStart', () =>
-	// 	{
-	// 		// Don't track until we've tracked on full page view.
-	// 		if ( this.firstPass ) {
-	// 			return;
-	// 		}
-
-	// 		// Store the current one so we can rollback if the state change fails.
-	// 		this.prev = this.referrer;
-	// 		this.referrer = this.currentUrl;
-	// 	} );
-
-	// 	$rootScope.$on( '$stateChangeSuccess', () =>
-	// 	{
-	// 		// We have finished the first state change.
-	// 		// We will now begin tracking new referrers.
-	// 		this.firstPass = false;
-	// 		this.currentUrl = window.location.href;
-	// 	} );
-
-	// 	$rootScope.$on( '$stateChangeError', () =>
-	// 	{
-	// 		// Rollback.
-	// 		this.referrer = this.prev;
-	// 	} );
-	// }
-
-	static get() {
+	static get referrer() {
 		if (!this.isInitialized) {
 			throw new Error(`Using Referrer before it's initialized.`);
 		}
 
-		return this.referrer;
+		return this._referrer;
 	}
 }
