75d09fd53a5bf5f0b178669a90fd12bb6277adb8
David DeCarmine <hworld@holo-world.com>
2017-07-26 11:43:28 -0600

    auth service fix for SSR

--- components/auth/auth.service.ts
+++ components/auth/auth.service.ts
@@ -2,6 +2,10 @@ import { Environment } from '../environment/environment.service';
 
 export class Auth {
 	static redirectDashboard() {
+		if (GJ_IS_SSR) {
+			return;
+		}
+
 		// This is mainly for client.
 		// It tells the intro animation that it should play the intro even if it can't find a user.
 		window.sessionStorage.setItem('client-intro-login-play', 'play');
