0f46c10c619f0ddd51e8a5a64dee462de6b4ff70
David DeCarmine <hworld@holo-world.com>
2017-08-08 19:15:08 -0600

    wait a bit before showing loading bar, show loading bar for router transitions while waiting for async chunks to load

--- components/loading/bar/bar.ts
+++ components/loading/bar/bar.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Axios from 'axios';
-import { Component, Watch } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import * as View from '!view!./bar.html?style=./bar.styl';
 
 @View
@@ -8,7 +8,9 @@ import * as View from '!view!./bar.html?style=./bar.styl';
 export class AppLoadingBar extends Vue {
 	requestCount = 0;
 	completedCount = 0;
+	shouldShow = false;
 
+	private showTimeout?: NodeJS.Timer;
 	private clearTimeout?: NodeJS.Timer;
 
 	get width() {
@@ -20,6 +22,18 @@ export class AppLoadingBar extends Vue {
 	}
 
 	mounted() {
+		// We hook into router so that we can show loading bar while the async
+		// component chunks are being loaded by webpack.
+		this.$router.beforeEach((_to, _from, next) => {
+			this.addRequest();
+			next();
+		});
+
+		this.$router.beforeResolve((_to, _from, next) => {
+			this.addComplete();
+			next();
+		});
+
 		Axios.interceptors.request.use(
 			config => {
 				this.addRequest(config);
@@ -43,20 +57,8 @@ export class AppLoadingBar extends Vue {
 		);
 	}
 
-	@Watch('requestCount')
-	@Watch('completedCount')
-	onCountChange() {
-		if (!this.requestCount) {
-			return;
-		}
-
-		if (this.completedCount >= this.requestCount) {
-			this.clear();
-		}
-	}
-
-	private addRequest(config: any) {
-		if (config.ignoreLoadingBar) {
+	private addRequest(config?: any) {
+		if (config && config.ignoreLoadingBar) {
 			return;
 		}
 
@@ -66,22 +68,41 @@ export class AppLoadingBar extends Vue {
 			this.clearTimeout = undefined;
 		}
 
+		if (!this.showTimeout) {
+			this.show();
+		}
+
 		++this.requestCount;
 	}
 
-	private addComplete(config: any) {
-		if (config.ignoreLoadingBar) {
+	private addComplete(config?: any) {
+		if (config && config.ignoreLoadingBar) {
 			return;
 		}
 
 		++this.completedCount;
+		if (this.completedCount >= this.requestCount) {
+			this.clear();
+		}
+	}
+
+	private show() {
+		this.showTimeout = setTimeout(() => {
+			this.shouldShow = true;
+		}, 300);
 	}
 
 	private clear() {
+		if (this.showTimeout) {
+			clearTimeout(this.showTimeout);
+			this.showTimeout = undefined;
+		}
+
 		// Wait for the 100% width to show first.
 		this.clearTimeout = setTimeout(() => {
 			this.requestCount = 0;
 			this.completedCount = 0;
+			this.shouldShow = false;
 		}, 300);
 	}
 }
