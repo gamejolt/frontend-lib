f95390a2f5da931e7ce0221956fc05fd11176bc3
David DeCarmine <hworld@holo-world.com>
2017-09-28 18:11:19 -0600

    user cards on avatar hover

--- components/user/user-avatar/user-avatar.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from 'ng-metadata/core';
-import { makeComponentProvider } from '../../../vue/angular-link';
-import { AppUserAvatar } from './user-avatar';
-
-@NgModule({
-	declarations: [makeComponentProvider(AppUserAvatar)],
-})
-export class UserAvatarModule {}
