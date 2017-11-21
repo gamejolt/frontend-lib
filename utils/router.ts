ad5f34e2a9519585dfb2d3369ce282072ddbbee7
David DeCarmine <hworld@holo-world.com>
2017-11-21 07:01:08 -0700

    It now correctly opens links in browser window in client when needed. It also supports ctrl-clicking a link to open it in new window finally. Closes https://github.com/gamejolt/gamejolt/issues/142

--- utils/router.ts
+++ utils/router.ts
@@ -1,10 +1,14 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { RouteConfig, Route, Location } from 'vue-router';
+import * as nwGui from 'nw.gui';
 
 import { routeError404, RouteError404 } from '../components/error/page/page.route';
 import { initScrollBehavior } from '../components/scroll/auto-scroll/autoscroll.service';
+import { Environment } from '../components/environment/environment.service';
 
-export function initRouter(appRoutes: VueRouter.RouteConfig[]) {
+const ClientBaseRegex = new RegExp('app:\\/\\/game\\-jolt\\-client\\/([^.]+)\\.html#');
+
+export function initRouter(appRoutes: RouteConfig[]) {
 	Vue.use(VueRouter);
 
 	const routes = [...appRoutes, routeError404];
@@ -28,7 +32,8 @@ export function hijackLinks(router: VueRouter, host: string) {
 
 	document.body.addEventListener('click', e => {
 		// Should we handle this event?
-		if (!guardHijackEvent(e)) {
+		const guard = guardHijackEvent(e);
+		if (guard === 'passthrough') {
 			return;
 		}
 
@@ -51,58 +56,111 @@ export function hijackLinks(router: VueRouter, host: string) {
 			return;
 		}
 
-		href = href.replace('http://' + host, '').replace('https://' + host, '');
+		// If client, get rid of the client base and replace it with the main domain.
+		href = href.replace(ClientBaseRegex, 'https://' + host);
+
+		// If we are supposed to open this in a new window, let's do that now.
+		if (guard === 'window') {
+			return newWindow(e, href);
+		}
+
+		const original = href;
+		const replacements = ['http://' + host, 'https://' + host];
+		for (const replacement of replacements) {
+			href = href.replace(replacement, '');
+		}
 
 		// Now try to match it against our routes and see if we got anything. If
 		// we match a 404 it's obviously wrong.
 		const matched = router.getMatchedComponents(href);
-		if (!matched.length || matched[0] === RouteError404) {
+		if (matched.length > 0 && matched[0] !== RouteError404) {
+			// We matched a route! Let's go to it and stop the browser from doing
+			// anything with the link click.
+			e.preventDefault();
+			router.push(href);
 			return;
 		}
 
-		// We matched a route! Let's go to it and stop the browser from doing
-		// anything with the link click.
-		e.preventDefault();
-		router.push(href);
+		if (GJ_IS_CLIENT) {
+			const isGameJoltPath = /^https?:\/\/gamejolt\./.test(href);
+			if (isGameJoltPath) {
+				const nonClientPaths = [
+					/^\/gas(\/.*)?/,
+					/^\/api(\/.*)?/,
+					/^\/developers(\/.*)?/,
+					/^\/claim(\/.*)?/,
+				];
+
+				const browsable = nonClientPaths.every(i => i.test(href) === false);
+				if (browsable) {
+					// Gotta rewrite the URL to include the correct base URL (to include the #).
+					// Otherwise it'll try to direct to the URL below as the raw URL.
+					window.location.href = Environment.wttfBaseUrl + href;
+					e.preventDefault();
+					return;
+				}
+			}
+
+			return newWindow(e, original);
+		}
+
+		// Let it direct to the URL by doing nothing.
 	});
 }
 
 // Basically taken from vue-router router-link. Decides if we should do any
 // logic against this particular event.
-function guardHijackEvent(e: any) {
+function guardHijackEvent(e: any): 'passthrough' | 'window' | 'handle' {
 	const ke = e as KeyboardEvent;
 	const me = e as MouseEvent;
 
-	// don't redirect with control keys
-	if (ke.metaKey || ke.altKey || ke.ctrlKey || ke.shiftKey) {
-		return false;
-	}
-
 	// don't redirect when preventDefault called
 	if (e.defaultPrevented) {
-		return false;
+		return 'passthrough';
+	}
+
+	// don't redirect with control keys
+	if (ke.metaKey || ke.altKey || ke.ctrlKey || ke.shiftKey) {
+		if (GJ_IS_CLIENT) {
+			// If in client, we should pop open a new window. Gotta make sure that if it's a
+			// client-relative link, we need to replace with the correct host.
+			return 'window';
+		}
+
+		return 'passthrough';
 	}
 
 	// don't redirect on right click
 	if (me.button !== undefined && me.button !== 0) {
-		return;
+		return 'passthrough';
 	}
 
 	// don't redirect if `target="_blank"`
 	if (e.currentTarget && e.currentTarget.getAttribute) {
 		const target = e.currentTarget.getAttribute('target');
 		if (/\b_blank\b/i.test(target)) {
-			return false;
+			// Client handles target="_blank" correctly.
+			return 'passthrough';
 		}
 	}
 
-	return true;
+	return 'handle';
+}
+
+function newWindow(e: Event, url: string) {
+	if (GJ_IS_CLIENT) {
+		const gui = require('nw.gui') as typeof nwGui;
+		gui.Shell.openExternal(url);
+		e.preventDefault();
+	} else {
+		window.open(url, '_blank');
+	}
 }
 
 export class LocationRedirect {
-	constructor(public location: VueRouter.Location) {}
+	constructor(public location: Location) {}
 
-	static fromRoute(from: VueRouter.Route, params: any, query: any = {}) {
+	static fromRoute(from: Route, params: any, query: any = {}) {
 		return new LocationRedirect({
 			name: from.name,
 			params: Object.assign({}, from.params, params),
@@ -113,7 +171,7 @@ export class LocationRedirect {
 	}
 }
 
-export function enforceLocation(route: VueRouter.Route, params: any, query: any = {}) {
+export function enforceLocation(route: Route, params: any, query: any = {}) {
 	for (const key in params) {
 		if (route.params[key] !== params[key]) {
 			return LocationRedirect.fromRoute(route, params, query);
