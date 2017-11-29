88ffc021ceb2bf7d25efcb50458e48c5d4a783e6
nilllzz <nidresch@googlemail.com>
2017-11-29 22:36:51 +0100

    implement for video thumbnails

--- components/comment/video/thumbnail/thumbnail.ts
+++ components/comment/video/thumbnail/thumbnail.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import View from '!view!./thumbnail.html?style=./thumbnail.styl';
 import './thumbnail-content.styl';
+import { State } from 'vuex-class';
 
 import { CommentVideo } from '../video-model';
 import { AppJolticon } from '../../../../vue/components/jolticon/jolticon';
@@ -10,6 +11,7 @@ import { AppTooltip } from '../../../tooltip/tooltip';
 import { CommentVideoModal } from '../modal/modal.service';
 import { AppUserAvatar } from '../../../user/user-avatar/user-avatar';
 import { AppUserFollowWidget } from '../../../user/follow-widget/follow-widget';
+import { Store } from '../../../../../../app/store/index';
 
 @View
 @Component({
@@ -25,7 +27,8 @@ import { AppUserFollowWidget } from '../../../user/follow-widget/follow-widget';
 })
 export class AppCommentVideoThumbnail extends Vue {
 	@Prop(CommentVideo) video: CommentVideo;
-	@Prop(Boolean) hideFollow?: boolean;
+
+	@State app: Store['app'];
 
 	isLoaded = false;
 
@@ -35,6 +38,10 @@ export class AppCommentVideoThumbnail extends Vue {
 
 	get user() {
 		return this.comment.user;
+	}
+
+	get shouldShowFollow() {
+		return !this.app.user || this.app.user.id !== this.user.id;
 	}
 
 	showModal() {
