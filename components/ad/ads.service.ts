3651fc76ade54ba49e13272781efcc48a9faf4eb
David DeCarmine <hworld@holo-world.com>
2018-02-14 18:01:52 -0700

    Revert "Allow to test amazon ads, but turn off by default"
    This reverts commit b159726721e64e06f789e640a3b1eb1735bbb076.

--- components/ad/ads.service.ts
+++ components/ad/ads.service.ts
@@ -204,16 +204,13 @@ export class Ads {
 
 		if (!DevDisabled) {
 			const prebidUnits = adsToDisplay.map(ad => Prebid.makeAdUnitFromSlot(ad.slot!));
-			const promises = [Prebid.getBids(prebidUnits)];
+			const apsSlots = adsToDisplay.map(ad => Aps.makeSlot(ad.slot!));
 
-			if (window.location.search.indexOf('AMAZON') !== -1) {
-				const apsSlots = adsToDisplay.map(ad => Aps.makeSlot(ad.slot!));
-				promises.push(Aps.getBids(apsSlots));
-			} else {
-				promises.push(Promise.resolve([]));
-			}
+			const [prebidBids, apsBids] = await Promise.all([
+				Prebid.getBids(prebidUnits),
+				Aps.getBids(apsSlots),
+			]);
 
-			const [prebidBids, apsBids] = await Promise.all<any>(promises);
 			this.storeBidTargeting(prebidBids, apsBids);
 		}
 
