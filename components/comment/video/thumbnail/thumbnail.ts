ec97872e5d373aa30d7a7064e56ea7173d82d775
David DeCarmine <hworld@holo-world.com>
2018-03-16 13:46:16 -0600

    some work

--- components/comment/video/thumbnail/thumbnail.ts
+++ components/comment/video/thumbnail/thumbnail.ts
@@ -5,20 +5,16 @@ import './thumbnail-content.styl';
 import { State } from 'vuex-class';
 
 import { CommentVideo } from '../video-model';
-import { AppJolticon } from '../../../../vue/components/jolticon/jolticon';
 import { AppTrackEvent } from '../../../analytics/track-event.directive.vue';
 import { AppTooltip } from '../../../tooltip/tooltip';
 import { CommentVideoModal } from '../modal/modal.service';
 import { AppUserAvatar } from '../../../user/user-avatar/user-avatar';
-import { AppUserFollowWidget } from '../../../user/follow-widget/follow-widget';
 import { Store } from '../../../../../../app/store/index';
 
 @View
 @Component({
 	components: {
-		AppJolticon,
 		AppUserAvatar,
-		AppUserFollowWidget,
 	},
 	directives: {
 		AppTrackEvent,
@@ -38,10 +34,6 @@ export class AppCommentVideoThumbnail extends Vue {
 
 	get user() {
 		return this.comment.user;
-	}
-
-	get shouldShowFollow() {
-		return !this.app.user || this.app.user.id !== this.user.id;
 	}
 
 	showModal() {
