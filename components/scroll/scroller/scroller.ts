bd53d9e553269f00b1298c355611bb422228d29a
David DeCarmine <hworld@holo-world.com>
2018-04-26 17:09:56 -0600

    Always show scrollbar on horizontal for overlay scrollers. Don't pop in scrollbars before loading in simplebar.

--- components/scroll/scroller/scroller.ts
+++ components/scroll/scroller/scroller.ts
@@ -11,11 +11,44 @@ export class AppScrollScroller extends Vue {
 	@Prop(Boolean) horizontal?: boolean;
 	@Prop(Boolean) hideScrollbar?: boolean;
 
+	isSimplebarBootstrapping = false;
+
+	// No watching.
+	simplebar?: SimpleBar;
+	private isDestroyed?: boolean;
+
+	get shouldOverlay() {
+		return this.overlay && !GJ_IS_SSR;
+	}
+
 	mounted() {
-		if (this.overlay) {
-			import('simplebar').then(SimpleBar => {
-				new SimpleBar(this.$el, { wrapContent: false, scrollbarMinSize: 30 });
-			});
+		if (!this.shouldOverlay) {
+			return;
 		}
+
+		// Since we should overlay, let's bootstrap simplebar in.
+		this.isSimplebarBootstrapping = true;
+
+		import('simplebar').then(SimpleBar => {
+			// Don't set it up if it's already destroyed.
+			if (this.isDestroyed) {
+				return;
+			}
+
+			this.simplebar = new SimpleBar(this.$el, {
+				wrapContent: false,
+				scrollbarMinSize: 30,
+				// Only autohide vertical scrollbars since they're easy to scroll with a
+				// mouse/trackpad.
+				autoHide: !this.horizontal,
+			});
+
+			this.isSimplebarBootstrapping = false;
+		});
+	}
+
+	destroyed() {
+		this.isDestroyed = true;
+		this.simplebar = undefined;
 	}
 }
