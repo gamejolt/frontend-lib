3cb057dd65e2c8cb984c2244152d1e8d5151108c
David DeCarmine <hworld@holo-world.com>
2018-08-20 14:38:12 -0700

    Bunch of changes/fixes

--- components/game/rating-growl/rating-growl.ts
+++ components/game/rating-growl/rating-growl.ts
@@ -1,10 +1,9 @@
+import View from '!view!./rating-growl.html';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import View from '!view!./rating-growl.html';
-
 import { AppRatingWidget } from '../../../../../app/components/rating/widget/widget';
+import { EventBus, EventBusDeregister } from '../../event-bus/event-bus.service';
 import { Game } from '../game.model';
-import { EventBus } from '../../event-bus/event-bus.service';
 
 @View
 @Component({
@@ -13,16 +12,25 @@ import { EventBus } from '../../event-bus/event-bus.service';
 	},
 })
 export class AppGameRatingGrowl extends Vue {
-	@Prop(Object) game: Game;
+	@Prop(Game)
+	game: Game;
+
+	private ratingWatchDeregister?: EventBusDeregister;
 
 	mounted() {
-		// Close the modal as soon as they rate the game. We set up on $on event
-		// so that we get notified even if they rate the game from the game page
-		// and not the modal.
-		EventBus.on('GameRating.changed', (gameId: number) => {
+		// Close the modal as soon as they rate the game. We set up on $on event so that we get
+		// notified even if they rate the game from the game page and not the modal.
+		this.ratingWatchDeregister = EventBus.on('GameRating.changed', (gameId: number) => {
 			if (gameId === this.game.id) {
 				this.$emit('close');
 			}
 		});
+	}
+
+	destroyed() {
+		if (this.ratingWatchDeregister) {
+			this.ratingWatchDeregister();
+			this.ratingWatchDeregister = undefined;
+		}
 	}
 }
