94ebf72f4864c7f1b3240352206797d7e4438339
David DeCarmine <hworld@holo-world.com>
2018-08-21 17:18:24 -0700

    Update rating growl

--- components/game/rating-growl/rating-growl.ts
+++ components/game/rating-growl/rating-growl.ts
@@ -1,7 +1,11 @@
 import View from '!view!./rating-growl.html';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import { AppRatingWidget } from '../../../../../app/components/rating/widget/widget';
+import {
+	AppRatingWidget,
+	RatingWidgetOnChange,
+	RatingWidgetOnChangePayload,
+} from '../../../../../app/components/rating/widget/widget';
 import { EventBus, EventBusDeregister } from '../../event-bus/event-bus.service';
 import { Game } from '../game.model';
 
@@ -13,18 +17,21 @@ import { Game } from '../game.model';
 })
 export class AppGameRatingGrowl extends Vue {
 	@Prop(Game)
-	game: Game;
+	game!: Game;
 
 	private ratingWatchDeregister?: EventBusDeregister;
 
 	mounted() {
 		// Close the modal as soon as they rate the game. We set up on $on event so that we get
 		// notified even if they rate the game from the game page and not the modal.
-		this.ratingWatchDeregister = EventBus.on('GameRating.changed', (gameId: number) => {
-			if (gameId === this.game.id) {
-				this.$emit('close');
+		this.ratingWatchDeregister = EventBus.on(
+			RatingWidgetOnChange,
+			(payload: RatingWidgetOnChangePayload) => {
+				if (payload.gameId === this.game.id) {
+					this.$emit('close');
+				}
 			}
-		});
+		);
 	}
 
 	destroyed() {
