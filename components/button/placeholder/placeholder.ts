d7821f20ce523ea6456207ac8dd53a01007bbe3e
David DeCarmine <hworld@holo-world.com>
2018-10-01 12:09:36 -0700

    Placeholder for user card

--- components/button/placeholder/placeholder.ts
+++ components/button/placeholder/placeholder.ts
@@ -1,10 +1,14 @@
+import View from '!view!./placeholder.html?style=./placeholder.styl';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import View from '!view!./placeholder.html?style=./placeholder.styl';
 
 @View
 @Component({})
 export class AppButtonPlaceholder extends Vue {
-	@Prop(Boolean) sparse?: boolean;
-	@Prop(Boolean) circle?: boolean;
+	@Prop(Boolean)
+	sparse?: boolean;
+	@Prop(Boolean)
+	circle?: boolean;
+	@Prop(Boolean)
+	block?: boolean;
 }
---
+++ components/user/card/placeholder/placeholder.ts
@@ -0,0 +1,12 @@
+import View from '!view!./placeholder.html?style=./placeholder.styl';
+import { AppButtonPlaceholder } from 'game-jolt-frontend-lib/components/button/placeholder/placeholder';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@View
+@Component({
+	components: {
+		AppButtonPlaceholder,
+	},
+})
+export class AppUserCardPlaceholder extends Vue {}
