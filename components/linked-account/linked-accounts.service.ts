ce93ffb7e5f69fbd7a69cf7af3d603cbfcad6bc9
David DeCarmine <hworld@holo-world.com>
2018-10-07 21:18:59 -0700

    Fix client linking/unlinking.

--- components/linked-account/linked-accounts.service.ts
+++ components/linked-account/linked-accounts.service.ts
@@ -11,60 +11,54 @@ export class LinkedAccounts {
 		resource: 'User' | 'Game',
 		resourceId: number | null = null
 	) {
-		if (GJ_IS_CLIENT) {
-			if (!provider) {
-				return;
-			}
-			return this.linkClient(router, provider, routeUrl);
-		}
-
 		let url = routeUrl + provider + '?resource=' + resource;
 		if (resourceId !== null) {
 			url += '&resourceId=' + resourceId;
 		}
+
+		if (GJ_IS_CLIENT) {
+			url += '&client';
+		}
+
 		const response = await Api.sendRequest(url, {});
-		Navigate.goto(response.redirectLocation);
-	}
 
-	static async linkClient(router: VueRouter, provider: Provider, routeUrl: string) {
-		const response = await Api.sendRequest(routeUrl + provider + '?client', {});
+		if (GJ_IS_CLIENT) {
+			// Gotta open a browser window for them to complete the sign up/login.
+			Navigate.gotoExternal(response.redirectLocation);
 
-		// Gotta open a browser window for them to complete the sign up/login.
-		Navigate.gotoExternal(response.redirectLocation);
-
-		// Now redirect them to the page that will continuously check if they
-		// are linked yet. We pass in the request token returned since this is
-		// what tells us our oauth state.
-		router.push({
-			name: 'dash.account.linked-accounts.linking',
-			query: { token: response.token },
-		});
+			// Now redirect them to the page that will continuously check if they
+			// are linked yet. We pass in the request token returned since this is
+			// what tells us our oauth state.
+			router.push({
+				name: 'dash.account.linked-accounts.linking',
+				query: { token: response.token },
+			});
+		} else {
+			Navigate.goto(response.redirectLocation);
+		}
 	}
 
 	static async login(router: VueRouter, provider: Provider) {
+		let url = '/web/auth/linked-accounts/link/' + provider;
 		if (GJ_IS_CLIENT) {
-			return this.loginClient(router, provider);
+			url += '?client';
 		}
 
-		const response = await Api.sendRequest('/web/auth/linked-accounts/link/' + provider, {});
-		Navigate.goto(response.redirectLocation);
-	}
+		const response = await Api.sendRequest(url, {});
 
-	static async loginClient(router: VueRouter, provider: Provider) {
-		const response = await Api.sendRequest(
-			'/web/auth/linked-accounts/link/' + provider + '?client',
-			{}
-		);
+		if (GJ_IS_CLIENT) {
+			// Gotta open a browser window for them to complete the sign up/login.
+			Navigate.gotoExternal(response.redirectLocation);
 
-		// Gotta open a browser window for them to complete the sign up/login.
-		Navigate.gotoExternal(response.redirectLocation);
-
-		// Now redirect them to the page that will continuously check if they
-		// are authed yet. We pass in the request token returned since this is
-		// what tells us our oauth state.
-		router.push({
-			name: 'auth.linked-account.poll',
-			params: { token: response.token },
-		});
+			// Now redirect them to the page that will continuously check if they
+			// are authed yet. We pass in the request token returned since this is
+			// what tells us our oauth state.
+			router.push({
+				name: 'auth.linked-account.poll',
+				params: { token: response.token },
+			});
+		} else {
+			Navigate.goto(response.redirectLocation);
+		}
 	}
 }
