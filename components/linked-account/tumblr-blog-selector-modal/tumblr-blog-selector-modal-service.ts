f18346474fdda42eed709167f9c178c6060c3541
David DeCarmine <hworld@holo-world.com>
2018-10-08 11:30:53 -0700

    Tweaked the tumblr blog selector modal

--- components/linked-account/tumblr-blog-selector-modal/tumblr-blog-selector-modal-service.ts
+++ components/linked-account/tumblr-blog-selector-modal/tumblr-blog-selector-modal-service.ts
@@ -3,14 +3,14 @@ import { Modal } from '../../modal/modal.service';
 import { LinkedAccount, TumblrBlog } from '../linked-account.model';
 
 export class ModalTumblrBlogSelector {
-	static async show(message: string, account: LinkedAccount, title = 'Confirm...') {
+	static async show(account: LinkedAccount, title = 'Confirm...') {
 		return await Modal.show<TumblrBlog | false>({
 			size: 'sm',
 			component: () =>
 				asyncComponentLoader(
 					import(/* webpackChunkName: "ModalTumblrBlogSelector" */ './tumblr-blog-selector-modal')
 				),
-			props: { message, account, title },
+			props: { account, title },
 		});
 	}
 }
