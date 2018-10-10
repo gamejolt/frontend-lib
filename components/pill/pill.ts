55a647f3d8f30fe5891a6a7f93e47ceb52d17804
David DeCarmine <hworld@holo-world.com>
2018-10-10 14:11:29 -0700

    Added a simple pill component.

---
+++ components/pill/pill.ts
@@ -0,0 +1,7 @@
+import View from '!view!./pill.html?style=./pill.styl';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@View
+@Component({})
+export class AppPill extends Vue {}
