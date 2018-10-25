0b00434c6cb33be6dbdab773940983e518955315
David DeCarmine <hworld@holo-world.com>
2018-10-24 17:33:44 -0700

    Fix payment source model to pull user address in.

--- components/payment-source/payment-source.model.ts
+++ components/payment-source/payment-source.model.ts
@@ -1,3 +1,4 @@
+import { UserAddress } from 'game-jolt-frontend-lib/components/user/address/address.model';
 import { Model } from '../model/model.service';
 
 export class PaymentSource extends Model {
@@ -6,6 +7,15 @@ export class PaymentSource extends Model {
 	exp_month!: number;
 	exp_year!: number;
 	created_on!: number;
+	user_address?: UserAddress;
+
+	constructor(data: any = {}) {
+		super(data);
+
+		if (data.user_address) {
+			this.user_address = new UserAddress(data.user_address);
+		}
+	}
 
 	$remove() {
 		return this.$_remove('/web/dash/payment-methods/remove/' + this.id);
