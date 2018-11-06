c2d7fbade870446c8dc7174972d8a0a72572019d
Nils <nidresch@googlemail.com>
2018-11-06 15:39:59 +0100

    formatting

--- components/comment/controls/controls.ts
+++ components/comment/controls/controls.ts
@@ -22,8 +22,10 @@ import { CommentVote } from '../vote/vote-model';
 	},
 })
 export class AppCommentControls extends Vue {
-	@Prop(Comment) comment!: Comment;
-	@Prop(Boolean) showReply?: boolean;
+	@Prop(Comment)
+	comment!: Comment;
+	@Prop(Boolean)
+	showReply?: boolean;
 
 	get votingTooltip() {
 		const userHasVoted = !!this.comment.user_vote;
