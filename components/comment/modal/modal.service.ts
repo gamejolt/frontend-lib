444c79df326ec91ecbda8c43cdd4dc765fb6793c
Nils <nidresch@googlemail.com>
2018-11-06 15:39:53 +0100

    remove permalink functionality from old modal

--- components/comment/modal/modal.service.ts
+++ components/comment/modal/modal.service.ts
@@ -1,9 +1,6 @@
-import { Modal } from '../../modal/modal.service';
 import { asyncComponentLoader } from '../../../utils/utils';
-import { Comment, fetchComment } from '../comment-model';
-import { VueRouter } from 'vue-router/types/router';
-import { Growls } from '../../growls/growls.service';
-import { Translate } from '../../translate/translate.service';
+import { Modal } from '../../modal/modal.service';
+import { Comment } from '../comment-model';
 
 interface CommentModalOptions {
 	resource?: string;
@@ -25,25 +22,5 @@ export class CommentModal {
 			},
 			size: 'sm',
 		});
-	}
-
-	static async checkPermalink(router: VueRouter) {
-		const hash = router.currentRoute.hash;
-		if (!hash || hash.indexOf('#comment-') !== 0) {
-			return;
-		}
-
-		const id = parseInt(hash.substring('#comment-'.length), 10);
-		if (!id) {
-			return;
-		}
-
-		const comment = await fetchComment(id);
-		if (!comment) {
-			Growls.error(Translate.$gettext(`Couldn't find comment. It may have been removed.`));
-			return;
-		}
-
-		CommentModal.show({ comment });
 	}
 }
