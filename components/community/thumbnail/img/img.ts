a8d5aca65e097030576d47915b357ab6e1976649
David DeCarmine <hworld@holo-world.com>
2018-12-19 14:05:52 -0800

    More styling stuff

--- components/community/thumbnail/img/img.ts
+++ components/community/thumbnail/img/img.ts
@@ -2,9 +2,14 @@ import View from '!view!./img.html';
 import { Community } from 'game-jolt-frontend-lib/components/community/community.model';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
+import { AppImgResponsive } from '../../../img/responsive/responsive';
 
 @View
-@Component({})
+@Component({
+	components: {
+		AppImgResponsive,
+	},
+})
 export class AppCommunityThumbnailImg extends Vue {
 	@Prop(Community)
 	community!: Community;
--- components/pill/pill.ts
+++ components/pill/pill.ts
@@ -1,7 +1,18 @@
 import View from '!view!./pill.html?style=./pill.styl';
 import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 
 @View
 @Component({})
-export class AppPill extends Vue {}
+export class AppPill extends Vue {
+	@Prop(Object)
+	to?: any;
+
+	get component() {
+		return this.to ? 'router-link' : 'div';
+	}
+
+	get hasImg() {
+		return !!this.$slots.img;
+	}
+}
