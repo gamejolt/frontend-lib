200f28a27e8e4b050898b38a11f71a956ce7fdcd
hworld <hworld@holo-world.com>
2019-01-16 15:57:04 -0600

    Removed a ton of ad code that we no longer need. (#98)

--- components/ad/widget/widget.ts
+++ components/ad/widget/widget.ts
@@ -1,14 +1,20 @@
 import View from '!view!./widget.html?style=./widget.styl';
-import { AppAd } from 'game-jolt-frontend-lib/components/ad/ad';
-import { Ads, hasPlaywire } from 'game-jolt-frontend-lib/components/ad/ads.service';
+import {
+	Ads,
+	AdSlotPos,
+	AdSlotPosValidator,
+} from 'game-jolt-frontend-lib/components/ad/ads.service';
 import { AppAdPlaywire } from 'game-jolt-frontend-lib/components/ad/playwire/playwire';
-import { AdSlotPos, AdSlotPosValidator } from 'game-jolt-frontend-lib/components/ad/slot';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import './widget-content.styl';
 
 @View
-@Component({})
+@Component({
+	components: {
+		AppAdPlaywire,
+	},
+})
 export class AppAdWidget extends Vue {
 	@Prop({ type: String, default: 'rectangle' })
 	size!: 'rectangle' | 'leaderboard';
@@ -21,9 +27,5 @@ export class AppAdWidget extends Vue {
 
 	get shouldShow() {
 		return Ads.shouldShow;
-	}
-
-	get adComponent() {
-		return hasPlaywire(this.$route) ? AppAdPlaywire : AppAd;
 	}
 }
