6d3ca07a898f39a418d9f6c6f152e93917aba16d
nilllzz <nidresch@googlemail.com>
2019-01-16 22:58:37 +0100

    Community Known Members (#95)
    * known users component
    
    * remove unused dep

---
+++ components/user/known/known.ts
@@ -0,0 +1,21 @@
+import View from '!view!./known.html?style=./known.styl';
+import { AppTooltip } from 'game-jolt-frontend-lib/components/tooltip/tooltip';
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import { AppUserAvatar } from '../user-avatar/user-avatar';
+import { User } from '../user.model';
+
+@View
+@Component({
+	components: {
+		AppUserAvatar,
+	},
+	directives: {
+		AppTooltip,
+	},
+})
+export class AppKnownUsers extends Vue {
+	@Prop(Array)
+	users!: User[];
+}
