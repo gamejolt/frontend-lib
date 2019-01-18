59b6660d0ef4a13b63fddfe5f2153247c939226c
David DeCarmine <hworld@holo-world.com>
2019-01-17 16:17:37 -0800

    Change from app-known-user to app-user-avatar-list

--- components/user/user-avatar/known/list.ts
+++ components/user/user-avatar/known/list.ts
@@ -1,10 +1,10 @@
-import View from '!view!./known.html?style=./known.styl';
+import View from '!view!./list.html?style=./list.styl';
 import { AppTooltip } from 'game-jolt-frontend-lib/components/tooltip/tooltip';
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
-import { AppUserAvatar } from '../user-avatar/user-avatar';
-import { User } from '../user.model';
+import { User } from '../../user.model';
+import { AppUserAvatar } from '../user-avatar';
 
 @View
 @Component({
@@ -15,7 +15,7 @@ import { User } from '../user.model';
 		AppTooltip,
 	},
 })
-export class AppKnownUsers extends Vue {
+export class AppUserAvatarList extends Vue {
 	@Prop(Array)
 	users!: User[];
 }
