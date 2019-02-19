9a347d52015d9b90ef8bcdd63c5dd0a635f850dd
Nils <nidresch@googlemail.com>
2019-02-19 20:06:22 +0100

    add new redirect to new user flow

--- components/auth/auth.service.ts
+++ components/auth/auth.service.ts
@@ -1,7 +1,19 @@
+import SectionService from '../../../../_common/sections/section.service';
 import { Environment } from '../environment/environment.service';
 import { Navigate } from '../navigate/navigate.service';
 
 export class Auth {
+	static redirectNewUserFlow() {
+		if (GJ_IS_SSR) {
+			return;
+		}
+
+		// This is mainly for client.
+		// It tells the intro animation that it should play the intro even if it can't find a user.
+		window.sessionStorage.setItem('client-intro-login-play', 'play');
+		SectionService.redirectTo('new-user.avatar');
+	}
+
 	static redirectDashboard() {
 		if (GJ_IS_SSR) {
 			return;
