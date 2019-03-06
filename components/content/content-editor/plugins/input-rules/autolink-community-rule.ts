d41433776d916bb0fa83cdd266a404943d15ac62
Nils <nidresch@googlemail.com>
2019-03-06 19:04:45 +0100

    add todo

--- components/content/content-editor/plugins/input-rules/autolink-community-rule.ts
+++ components/content/content-editor/plugins/input-rules/autolink-community-rule.ts
@@ -4,7 +4,7 @@ import { EditorState } from 'prosemirror-state';
 
 export function autolinkCommunityRule() {
 	return new InputRule(
-		/(?:^|\W)c\/([a-z0-9-_]{3,50})$/i,
+		/(?:^|\W)c\/([a-z0-9-_]{3,50})$/i, // TODO: adjust this to match our community path ruleset
 		(state: EditorState<any>, match: string[], start: number, end: number) => {
 			if (match.length === 2) {
 				const tr = state.tr;
@@ -12,9 +12,10 @@ export function autolinkCommunityRule() {
 				tr.insertText(match[0], start, end);
 				// Removes all previous links on this text
 				tr.removeMark(start, end, state.schema.marks.link);
+				const communityPath = match[1];
 				const linkMark = (state.schema.marks.link as MarkType).create({
-					href: '/c/' + match[1],
-					title: 'Community: ' + match[1],
+					href: '/c/' + communityPath,
+					title: 'Community: ' + communityPath,
 				});
 				// Add the link mark to the text. +1 to include the new char.
 				tr.addMark(start, end + 1, linkMark);
