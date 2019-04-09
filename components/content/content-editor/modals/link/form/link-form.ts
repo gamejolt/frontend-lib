31cc9fa01ddf05b075722aff05b30fae28dc4de6
Nils <nidresch@googlemail.com>
2019-04-09 04:10:24 +0200

    autofocus href input on link form

--- components/content/content-editor/modals/link/form/link-form.ts
+++ components/content/content-editor/modals/link/form/link-form.ts
@@ -1,10 +1,15 @@
 import View from '!view!./link-form.html';
 import { LinkData } from 'game-jolt-frontend-lib/components/content/content-editor/modals/link/link-modal.service';
+import { AppFocusWhen } from 'game-jolt-frontend-lib/components/form-vue/focus-when.directive';
 import { BaseForm, FormOnInit } from 'game-jolt-frontend-lib/components/form-vue/form.service';
 import { Component } from 'vue-property-decorator';
 
 @View
-@Component({})
+@Component({
+	directives: {
+		AppFocusWhen,
+	},
+})
 export class FormContentEditorLink extends BaseForm<LinkData> implements FormOnInit {
 	get valid() {
 		return this.formModel.href.length > 0;
