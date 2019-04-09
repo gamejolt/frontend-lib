47b5589895c17528f68a106acb70fbf67c2cd17f
Nils <nidresch@googlemail.com>
2019-04-09 04:10:17 +0200

    Add link mark on Ctrl K

--- components/content/content-editor/keymap.ts
+++ components/content/content-editor/keymap.ts
@@ -1,9 +1,12 @@
 import { AppContentEditor } from 'game-jolt-frontend-lib/components/content/content-editor/content-editor';
+import { ContentEditorService } from 'game-jolt-frontend-lib/components/content/content-editor/content-editor.service';
 import { ContentListService } from 'game-jolt-frontend-lib/components/content/content-editor/content-list.service';
+import { ContentEditorLinkModal } from 'game-jolt-frontend-lib/components/content/content-editor/modals/link/link-modal.service';
 import { chainCommands, exitCode, toggleMark } from 'prosemirror-commands';
 import { redo, undo } from 'prosemirror-history';
 import { Schema } from 'prosemirror-model';
 import { sinkListItem, splitListItem } from 'prosemirror-schema-list';
+import { EditorState, Transaction } from 'prosemirror-state';
 
 export function getContentEditorKeymap(editor: AppContentEditor, schema: Schema) {
 	const isMac = typeof navigator != 'undefined' ? /Mac/.test(navigator.platform) : false;
@@ -22,10 +25,26 @@ export function getContentEditorKeymap(editor: AppContentEditor, schema: Schema)
 			);
 			return true;
 		}),
+		// open emoji panel
 		'Mod-e': () => {
 			if (editor.capabilities.gjEmoji) {
-				// Show emoji panel
 				editor.showEmojiPanel();
+			}
+			return true;
+		},
+		// Add/remove link
+		'Mod-k': async (state: EditorState, dispatch: ((tr: Transaction) => void)) => {
+			const selectionMarks = ContentEditorService.getSelectionMarks(state);
+			if (selectionMarks.some(m => m.type.name === 'link')) {
+				toggleMark(schema.marks.link);
+			} else {
+				const result = await ContentEditorLinkModal.show();
+				if (result) {
+					toggleMark(schema.marks.link, {
+						href: result.href,
+						title: result.title,
+					})(state, dispatch);
+				}
 			}
 			return true;
 		},
