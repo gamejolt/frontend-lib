ae9615f3e9dea4f15fa38f940d285b359c89b224
Nils <nidresch@googlemail.com>
2019-04-30 01:31:37 +0200

    add prism js to content viewer's code block

--- components/content/content-viewer/components/code/code.ts
+++ components/content/content-viewer/components/code/code.ts
@@ -1,9 +1,31 @@
+import 'prismjs/themes/prism.css';
 import Vue, { CreateElement } from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { ContentObject } from '../../../content-object';
 import { ContentOwner } from '../../../content-owner';
-import { renderChildren } from '../base-component';
-import './code.css';
+import './code-style.css';
+import './code.styl';
+require('prismjs');
+const Prism = require('vue-prism-component');
+
+const LANGUAGE_MAP = {
+	js: 'javascript',
+	javascript: 'javascript',
+	ts: 'javascript',
+	typescript: 'javascript',
+
+	css: 'css',
+
+	html: 'markup',
+	markup: 'markup',
+	xml: 'markup',
+	svg: 'markup',
+
+	c: 'clike',
+	csharp: 'clike',
+	'c++': 'clike',
+	java: 'clike',
+} as any;
 
 @Component({})
 export class AppContentViewerCodeBlock extends Vue {
@@ -13,10 +35,41 @@ export class AppContentViewerCodeBlock extends Vue {
 	owner!: ContentOwner;
 
 	render(h: CreateElement) {
+		let text = '';
+		let language = 'clike';
+
+		if (this.data.content.length > 0) {
+			text = this.data.content[0].text || '';
+		}
+
+		const annotation = this.getLanguageAnnotation(text);
+		if (annotation !== undefined) {
+			const annotatedLanguage = LANGUAGE_MAP[annotation];
+			if (annotatedLanguage !== undefined) {
+				language = annotatedLanguage;
+				text = text.slice(annotation.length + 2); // + 2 to remove the # and new line
+			}
+		}
+
 		return h(
-			'pre',
-			{ class: 'content-viewer-code-block' },
-			renderChildren(h, this.owner, this.data.content)
+			Prism,
+			{
+				class: 'content-viewer-code-block',
+				props: {
+					language,
+				},
+			},
+			text
 		);
+	}
+
+	private getLanguageAnnotation(text: string) {
+		if (text.length > 0) {
+			const match = text.match(/#(.+)\n/);
+			if (match !== null && match.length > 1) {
+				const matchedText = match[1];
+				return matchedText;
+			}
+		}
 	}
 }
