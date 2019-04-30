bf7b2e33ff60625b5ed5c11fa707f80cc3a1d4a5
Nils <nidresch@googlemail.com>
2019-04-30 02:23:53 +0200

    load in prism dynamically

--- components/content/content-viewer/components/code/code.ts
+++ components/content/content-viewer/components/code/code.ts
@@ -1,11 +1,9 @@
-import 'prismjs/themes/prism.css';
 import Vue, { CreateElement } from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { ContentObject } from '../../../content-object';
 import { ContentOwner } from '../../../content-owner';
-import './code-style.css';
+import { renderChildren } from '../base-component';
 import './code.styl';
-require('prismjs');
 const Prism = require('vue-prism-component');
 
 const LANGUAGE_MAP = {
@@ -34,7 +32,30 @@ export class AppContentViewerCodeBlock extends Vue {
 	@Prop(Object)
 	owner!: ContentOwner;
 
+	isPrismLoaded = false;
+
+	async mounted() {
+		await Promise.all([
+			import(/* webpackChunkName: "prismjs" */ 'prismjs'),
+			import(/* webpackChunkName: "prismjs" */ 'prismjs/themes/prism.css' as any),
+		]);
+		await import(/* webpackChunkName: "prismjs" */ './code-style.css' as any);
+		this.isPrismLoaded = true;
+	}
+
 	render(h: CreateElement) {
+		if (this.isPrismLoaded) {
+			return this.renderPrism(h);
+		} else {
+			return h(
+				'pre',
+				{ class: 'content-viewer-code-block' },
+				renderChildren(h, this.owner, this.data.content)
+			);
+		}
+	}
+
+	private renderPrism(h: CreateElement) {
 		let text = '';
 		let language = 'clike';
 
