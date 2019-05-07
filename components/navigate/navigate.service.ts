46de8a5c3bd6535caa1711b671504f43d2414994
YLivay <yarivlivay@gmail.com>
2019-05-08 02:20:27 +0300

    Fully revert navigate.goto changes

--- components/navigate/navigate.service.ts
+++ components/navigate/navigate.service.ts
@@ -55,66 +55,11 @@ export class Navigate {
 		}
 	}
 
-	private static gotoHard(href: string) {
-		console.log('Hard redirecting');
+	private static goto(href: string) {
 		this.redirecting = true;
 
 		this.callDestructors(href);
 		window.location.href = href;
-	}
-
-	/**
-	 * Goes to a url. By default it'll try doing a soft redirect using the section router if possible.
-	 *
-	 * @param href The url to redirect to
-	 * @param forceHardRedirect True if to always to a hard redirect,
-	 * even if it can be redirected in the same route without reloading the entire SPA.
-	 */
-	static goto(href: string, forceHardRedirect?: boolean) {
-		this.gotoHard(href);
-		return;
-
-		// if (forceHardRedirect || !this.router) {
-		// 	this.gotoHard(href);
-		// 	return;
-		// }
-
-		// const current = window.location.href;
-
-		// // This should never happen, if it does it's an error but we'll
-		// // redirect anyways and let the browser try handling this.
-		// if (!current.endsWith(this.router.currentRoute.fullPath)) {
-		// 	this.gotoHard(href);
-		// 	return;
-		// }
-
-		// // Find the base url by removing the current route's fullPath from it.
-		// // We should be left with the base url without the trailing /
-		// const baseUrl = current.substring(
-		// 	0,
-		// 	current.length - this.router.currentRoute.fullPath.length
-		// );
-
-		// // If the url we're going to isn't under our base url - hard redirect.
-		// if (!href.startsWith(baseUrl)) {
-		// 	this.gotoHard(href);
-		// 	return;
-		// }
-
-		// const routeHref = href.substring(baseUrl.length);
-
-		// // If our router doesn't know how to resolve this url, let the browser try.
-		// const matched = this.router.getMatchedComponents(routeHref);
-		// if (matched.length === 0 || matched[0].name === 'RouteError404') {
-		// 	this.gotoHard(href);
-		// 	return;
-		// }
-
-		// console.log('Soft redirecting to ' + routeHref);
-
-		// // If the url we're given is exactly the base url, routeHref will end up being an empty string.
-		// // To resolve to the correct route in this case, we need to pass /
-		// this.router.push(routeHref || '/');
 	}
 
 	static gotoExternal(href: string) {
