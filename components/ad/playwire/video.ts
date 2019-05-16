361b512ee053964f0a7b04dfad00f788a9f5dc25
David DeCarmine <hworld@holo-world.com>
2019-05-16 12:03:39 -0700

    Log ad ticks for video ads.

--- components/ad/playwire/video.ts
+++ components/ad/playwire/video.ts
@@ -1,27 +1,31 @@
+import { Ads } from 'game-jolt-frontend-lib/components/ad/ads.service';
 import { Playwire } from 'game-jolt-frontend-lib/components/ad/playwire/playwire.service';
+import { FiresidePost } from 'game-jolt-frontend-lib/components/fireside/post/post-model';
+import { Game } from 'game-jolt-frontend-lib/components/game/game.model';
+import { User } from 'game-jolt-frontend-lib/components/user/user.model';
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
 @Component({})
 export default class AppAdPlaywireVideo extends Vue {
-	// get resourceInfo() {
-	// 	let resource: string = undefined as any;
-	// 	let resourceId: number = undefined as any;
+	get resourceInfo() {
+		let resource: string = undefined as any;
+		let resourceId: number = undefined as any;
 
-	// 	const adResource = Ads.settings.resource;
-	// 	if (adResource instanceof Game) {
-	// 		resource = 'Game';
-	// 		resourceId = adResource.id;
-	// 	} else if (adResource instanceof User) {
-	// 		resource = 'User';
-	// 		resourceId = adResource.id;
-	// 	} else if (adResource instanceof FiresidePost) {
-	// 		resource = 'Fireside_Post';
-	// 		resourceId = adResource.id;
-	// 	}
+		const adResource = Ads.settings.resource;
+		if (adResource instanceof Game) {
+			resource = 'Game';
+			resourceId = adResource.id;
+		} else if (adResource instanceof User) {
+			resource = 'User';
+			resourceId = adResource.id;
+		} else if (adResource instanceof FiresidePost) {
+			resource = 'Fireside_Post';
+			resourceId = adResource.id;
+		}
 
-	// 	return { resource, resourceId };
-	// }
+		return { resource, resourceId };
+	}
 
 	mounted() {
 		Playwire.addAd(this);
@@ -34,19 +38,18 @@ export default class AppAdPlaywireVideo extends Vue {
 	display() {
 		this.renderScript();
 
-		// // Log that we viewed this ad immediately.
-		// this.sendBeacon(Ads.EVENT_VIEW);
+		// Log that we viewed this ad immediately.
+		this.sendBeacon(Ads.EVENT_VIEW);
 	}
 
-	// No need to do this right now for testing.
-	// private sendBeacon(event: string) {
-	// 	Ads.sendBeacon(
-	// 		event,
-	// 		Ads.TYPE_DISPLAY,
-	// 		this.resourceInfo.resource,
-	// 		this.resourceInfo.resourceId
-	// 	);
-	// }
+	private sendBeacon(event: string) {
+		Ads.sendBeacon(
+			event,
+			Ads.TYPE_DISPLAY,
+			this.resourceInfo.resource,
+			this.resourceInfo.resourceId
+		);
+	}
 
 	private renderScript() {
 		const script = window.document.createElement('script');
