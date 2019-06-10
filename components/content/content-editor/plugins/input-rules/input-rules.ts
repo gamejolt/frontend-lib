dc807c65e34abea109e2e25f1c501cf2afa4601b
Nils <nidresch@googlemail.com>
2019-06-10 23:12:57 +0200

    remove community autolinking

--- components/content/content-editor/plugins/input-rules/input-rules.ts
+++ components/content/content-editor/plugins/input-rules/input-rules.ts
@@ -1,6 +1,5 @@
 import { InputRule, inputRules } from 'prosemirror-inputrules';
 import { ContextCapabilities } from '../../../content-context';
-// import { autolinkCommunityRule } from './autolink-community-rule';
 import { insertBulletListRule } from './insert-bullet-list-rule';
 import { insertEmojiRule } from './insert-emoji-rule';
 import { insertOrderedListRule } from './insert-ordered-list-rule';
@@ -10,9 +9,6 @@ export function createInputRules(capabilities: ContextCapabilities) {
 
 	if (capabilities.emoji) {
 		rules.push(insertEmojiRule());
-	}
-	if (capabilities.textLink) {
-		// rules.push(autolinkCommunityRule());
 	}
 	if (capabilities.list) {
 		rules.push(insertOrderedListRule());
