20f9b7b525427d398ab607c37d97779944608e25
Nils <nidresch@googlemail.com>
2019-07-08 13:53:28 +0200

    remove gj staff icon

--- components/user/verified-tick/verified-tick.ts
+++ components/user/verified-tick/verified-tick.ts
@@ -26,21 +26,17 @@ export default class AppUserVerifiedTick extends Vue {
 	verticalAlign!: boolean;
 
 	get shouldShow() {
-		return this.user.is_verified || this.user.isMod;
+		return this.user.is_verified;
 	}
 
 	get icon() {
-		if (this.user.isMod) {
-			return 'gamejolt';
-		} else if (this.user.is_verified) {
+		if (this.user.is_verified) {
 			return 'verified';
 		}
 	}
 
 	get tooltip() {
-		if (this.user.isMod) {
-			return this.$gettext('Game Jolt Staff');
-		} else if (this.user.is_verified) {
+		if (this.user.is_verified) {
 			return this.$gettext('Verified Account');
 		}
 	}
